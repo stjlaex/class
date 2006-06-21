22daeec8cbc99cf3cedd08ac11db04f731a78a06
stjohnson <stj@laex.org>
2006-06-21 19:53:11 +0000

    completed reportentry delete

--- classdata.php
+++ classdata.php
@@ -2,7 +2,7 @@
 require_once('include.php');
 $CFG->books=$books;
 $CFG->roles=$roles;
-$CFG->version='ClaSS-0.7.25 (dev)';
+$CFG->version='ClaSS-0.7.26 (dev)';
 $CFG->dirroot=$CFG->installpath.'/'.$CFG->applicationdirectory;
 global $CFG;
 $session='ClaSS'.$CFG->shortname;
