017a4d64f637218bd4c32e90c8ae3d1d4d14153a
stjohnson <stj@laex.org>
2006-06-21 19:46:33 +0000

    completed reportentry delete

--- js/formfunctions.js
+++ js/formfunctions.js
@@ -100,7 +100,6 @@ function clickToAction(buttonObject){
 		}
 	else if(action=='current'){
 		var recordId=xmlRecord.childNodes[1].childNodes[0].nodeValue;
-alert(recordId);
 		var script=buttonObject.value;
 		var url=pathtobook + "httpscripts/" + script + "?uniqueid=" + escape(recordId);
 		var answer=confirmAction(buttonObject.title);
