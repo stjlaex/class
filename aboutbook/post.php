e0e5a4ec4031e0ccb8e852cb093582934774caf0
stjohnson <stj@laex.org>
2006-10-31 16:17:37 +0000

    about emails changed

--- aboutbook/post.php
+++ aboutbook/post.php
@@ -5,9 +5,9 @@
 $action='about.php';
 $choice='';
 
-$summary=clean_text($_POST{'summary'});
-if(isset($_POST{'book'})){$book=$_POST{'book'};}else{$book='';}
-$detail=clean_text($_POST{'detail'});
+$summary=clean_text($_POST['summary']);
+if(isset($_POST['book'])){$book=$_POST['book'];}else{$book='';}
+$detail=clean_text($_POST['detail']);
 $entrydate=$_POST['date0'];
 $subject=clean_text($_POST['subject']); 
 $address=$_POST['address'];
@@ -19,13 +19,22 @@ include('scripts/sub_action.php');
 	$message=$subject."\r\n".'Date:
 	'.$entrydate."\r\n".$CFG->version."\r\n". $book. "\r\n". 
 				$detail. "\r\n" .'Posted by '.$tid. "\r\n" .$footer;
+
+	if(isset($CFG->clientid)){
+		$clientid=$CFG->clientid;
+		if($queue=='class-bug'){$queue='class_bug';}
+		if($queue=='class-feature'){$queue='class_feature';}
+		}
+
 	$recipients[]=array('email'=> $queue.'@'.$CFG->support,
 					'username'=>'ClaSS Support');
 	if(isset($CFG->contact) and $queue=='support'){
 		$recipients[]=array('email'=> $CFG->contact,'username'=>'ClaSS Contact');
 		}
+
 	$headers='From: ClaSS@'.$CFG->siteaddress . "\r\n" . 
 				'Reply-To: noreply@'.$CFG->siteaddress . "\r\n" .
+				'List-Id: '.$clientid . "\r\n" .
 			    'X-Mailer: PHP/' . phpversion();
 
    	foreach($recipients as $key => $recipient){
