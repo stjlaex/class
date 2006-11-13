08586bc3ad6388bbad5407d1222ac9bb2d66a5f4
stjohnson <stj@laex.org>
2006-11-13 11:59:10 +0000

    bugfix student_reports

--- scripts/list_student_report.php
+++ scripts/list_student_report.php
@@ -10,14 +10,20 @@
 				rideid.assessment_id=eidsid.assessment_id WHERE eidsid.student_id='$sid'
 			    ORDER BY report_id DESC");
 	$reports=array();
+	$reportwrappers=array();
 	while($rideid=mysql_fetch_array($d_rideid,MYSQL_ASSOC)){
 		$rid=$rideid['report_id'];
+		$d_report=mysql_query("SELECT id, title, course_id, date FROM report 
+			    WHERE id='$rid' ORDER BY date DESC, title");
+		while($report=mysql_fetch_array($d_report,MYSQL_ASSOC)){
+			$reports[$report['id']]=$report;
+			}
 		$d_report=mysql_query("SELECT id, title, date FROM report JOIN
 				ridcatid ON ridcatid.report_id=report.id  
 			    WHERE ridcatid.categorydef_id='$rid' 
 				AND ridcatid.subject_id='wrapper' ORDER BY date DESC, title");
 		while($report=mysql_fetch_array($d_report,MYSQL_ASSOC)){
-			$reports[$report['id']]=mysql_fetch_array($d_report,MYSQL_ASSOC);
+			$reportwrappers[$report['id']]=$report;
 			}
 		}
 ?>
@@ -26,6 +32,18 @@
   <label for="Current Reports"><?php print_string('current');?></label>
   <select style="width:60%;" id="Current Reports" type="text" name="wrapper_rid"
 			tabindex="<?php print $tab++;?>" size="4">
+<?php
+	while(list($rid,$report)=each($reportwrappers)){
+		if($report['date']>=$todate){
+?>
+		<option value="<?php print $report['id'];?>">
+			<?php print $report['title'].' ('.$report['date'].')';?>
+		</option>
+<?php
+			}
+ 		}
+?>
+		<option value="">--------------------</option>
 <?php
    	while(list($rid,$report)=each($reports)){
 		if($report['date']>=$todate){
@@ -40,10 +58,23 @@
   </select>
 </div>
 
-<div class="center"> 
+<div class="center">
   <label for="Previous"><?php print_string('previous');?></label>
   <select style="width:60%;" id="Previous" type="text" name="wrapper_rid"
-			size="8" multiple="multiple" tabindex="<?php print $tab++;?>">
+			size="18" multiple="multiple" tabindex="<?php print $tab++;?>">
+<?php
+	reset($reportwrappers);
+	while(list($rid,$report)=each($reportwrappers)){
+		if($report['date']<$todate){
+?>
+		<option value="<?php print $report['id'];?>">
+			<?php print $report['title'].' ('.$report['date'].')';?>
+		</option>
+<?php
+			}
+ 		}
+?>
+		<option value="">--------------------</option>
 <?php
 	reset($reports);
 	while(list($rid,$report)=each($reports)){
