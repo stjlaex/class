1540f460be1836a242aee0a57523e02d5784915c
stjohnson <stj@laex.org>
2007-02-28 14:26:23 +0000

    bugfix to sentype table

--- classdata.php
+++ classdata.php
@@ -2,7 +2,7 @@
 require_once('include.php');
 $CFG->books=$books;
 $CFG->roles=$roles;
-$CFG->version='ClaSS-0.8.11';
+$CFG->version='ClaSS-0.8.10';
 $CFG->dirroot=$CFG->installpath.'/'.$CFG->applicationdirectory;
 global $CFG;
 $session='ClaSS'.$CFG->shortname;
