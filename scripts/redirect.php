9984d0ad384fb15386dcb719c3811779798e3445
stjohnson <stj@laex.org>
2007-05-08 16:32:16 +0000

    moving redirect across to action_post_vars

--- scripts/redirect.php
+++ scripts/redirect.php
@@ -1,6 +1,8 @@
 <?php
 /**											scripts/redirect.php
  *
+ * Fill the array $action_post_vars with any variable names to be posted
+ *
  */
 if(!isset($pausetime)){$pausetime=150;}
 ?>
@@ -9,6 +11,7 @@ if(!isset($pausetime)){$pausetime=150;}
 	<input type="hidden" name="current" value="<?php if(isset($action)){print $action;}?>" />
 	<input type="hidden" name="cancel" value="<?php if(isset($cancel)){print $cancel;}?>" />
 	<input type="hidden" name="choice" value="<?php if(isset($choice)){print $choice;}?>" />
+
 	<input type="hidden" name="cid" value="<?php if(isset($cid)){print $cid;}?>" />
 	<input type="hidden" name="sid" value="<?php if(isset($sid)){print $sid;}?>" />
 	<input type="hidden" name="checkmid[]" value="<?php if(isset($mid)){print $mid;}?>" />
@@ -18,16 +21,15 @@ if(!isset($pausetime)){$pausetime=150;}
 	<input type="hidden" name="bid" value="<?php if(isset($bid)){print $bid;}?>" />
 <?php
 if(isset($rids)){
-	while(list($index, $rid) = each($rids)){
+	while(list($index, $rid)=each($rids)){
 ?>
 	 	<input type="hidden" name="rids[]" value="<?php print $rid;?>">
 <?php
 		}
 	}
-
-if(isset($coversheet)){
+if(isset($displaymid)){
 ?>
-	 	<input type="hidden" name="coversheet" value="<?php print $coversheet;?>">
+	 	<input type="hidden" name="displaymid" value="<?php print $displaymid;?>">
 <?php
 	}
 if(isset($date)){
@@ -50,115 +52,9 @@ if(isset($date1)){
 	 	<input type="hidden" name="date1" value="<?php print $date1;?>">
 <?php
 	}
-if(isset($selbid)){
-?>
-	 	<input type="hidden" name="selbid" value="<?php print $selbid;?>">
-<?php
-	}
-if(isset($tagname)){
-?>
-	 	<input type="hidden" name="tagname" value="<?php print $tagname;?>">
-<?php
-	}
-if(isset($selcrid)){
-?>
-	 	<input type="hidden" name="selcrid" value="<?php print $selcrid;?>">
-<?php
-	}
-if(isset($seluid)){
-?>
-	 	<input type="hidden" name="seluid" value="<?php print $seluid;?>">
-<?php
-	}
-if(isset($displaymid)){
-?>
-	 	<input type="hidden" name="displaymid" value="<?php print $displaymid;?>">
-<?php
-	}
-if(isset($gena)){
-?>
-	 	<input type="hidden" name="gena" value="<?php print $gena;?>">
-<?php
-	}
-if(isset($lena)){
-?>
-	 	<input type="hidden" name="lena" value="<?php print $lena;?>">
-<?php
-	}
-if(isset($comment)){
-?>
-	 	<input type="hidden" name="comment" value="<?php print $comment;?>">
-<?php
-	}
-if(isset($grades)){
-?>
-	 	<input type="hidden" name="grades" value="<?php print $grades;?>">
-<?php
-	}
-if(isset($newcid)){
-?>
-	 	<input type="hidden" name="newcid" value="<?php print $newcid;?>">
-<?php
-	}
-if(isset($newtid)){
-?>
-	 	<input type="hidden" name="newtid" value="<?php print $newtid;?>">
-<?php
-	}
-if(isset($newfid)){
-?>
-	 	<input type="hidden" name="newfid" value="<?php print $newfid;?>">
-<?php
-	}
-if(isset($newyid)){
-?>
-	 	<input type="hidden" name="newyid" value="<?php print $newyid;?>">
-<?php
-	}
-if(isset($comid)){
-?>
-	 	<input type="hidden" name="comid" value="<?php print $comid;?>">
-<?php
-	}
-if(isset($newcomid)){
-?>
-	 	<input type="hidden" name="newcomid" value="<?php print $newcomid;?>">
-<?php
-	}
-if(isset($newcomtype)){
-?>
-	 	<input type="hidden" name="newcomtype" value="<?php print $newcomtype;?>">
-<?php
-	}
-if(isset($comtype)){
-?>
-	 	<input type="hidden" name="comtype" value="<?php print $comtype;?>">
-<?php
-	}
-if(isset($comname)){
-?>
-	 	<input type="hidden" name="comname" value="<?php print $comname;?>">
-<?php
-	}
-if(isset($contactno)){
-?>
-	 	<input type="hidden" name="contactno" value="<?php print $contactno;?>">
-<?php
-	}
-if(isset($checkeveid)){
-?>
-	 	<input type="hidden" name="checkeveid" value="<?php print $checkeveid;?>">
-<?php
-	}
-if(isset($startday)){
-?>
-	 	<input type="hidden" name="startday" value="<?php print $startday;?>">
-<?php
-	}
-if(isset($pregid)){
-?>
-	 	<input type="hidden" name="pregid" value="<?php print $pregid;?>">
-<?php
+
+if(isset($action_post_vars)){
+	include('scripts/set_action_post_vars.php');
 	}
 ?>
 
---
+++ scripts/set_action_post_vars.php
@@ -0,0 +1,27 @@
+<?php
+/**								scripts/set_action_post_vars.php
+ *
+ * For any page variables which need to be posted as part of an action
+ * pages redirect.
+ * The vars are listed in array $action_post_vars and can include
+ * numericaly indexed arrays.
+ */
+while(list($index,$varname)=each($action_post_vars)){
+	if(isset($$varname)){
+		if(is_array($$varname)){
+			reset($$varname);
+			while(list($index,$value)=each($$varname)){
+?>
+	 	<input type="hidden" name="<?php print $varname;?>[]" value="<?php print $value;?>">
+<?php
+				 }
+			}
+		else{
+			if($book=='admin'){trigger_error($varname.': '.$$varname,E_USER_WARNING);}
+?>
+		<input type="hidden" name="<?php print $varname;?>" value="<?php print $$varname;?>">
+<?php
+			}
+		}
+	}
+?>
