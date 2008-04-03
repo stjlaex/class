3b0f226184f6ff7db1d258ef1285097bedad335c
stjohnson <stj@laex.org>
2008-04-03 15:42:17 +0000

    changes to order permissions

--- classdata.php
+++ classdata.php
@@ -2,7 +2,7 @@
 require_once('include.php');
 $CFG->books=$books;
 $CFG->roles=$roles;
-$CFG->version='ClaSS-0.8.43';
+$CFG->version='ClaSS-0.8.42';
 $CFG->dirroot=$CFG->installpath.'/'.$CFG->applicationdirectory;
 global $CFG;
 $session='ClaSS'.$CFG->shortname;
