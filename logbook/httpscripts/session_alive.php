e5f4725c0de30d1565cb12b5747d65925e6b12d2
stjohnson <stj@laex.org>
2008-04-09 16:10:35 +0000

    session_alive script

---
+++ logbook/httpscripts/session_alive.php
@@ -0,0 +1,30 @@
+<?php
+/**                    httpscripts/session_alive.php
+ */
+
+require_once('../../scripts/http_head_options.php');
+
+if(!isset($xmlid)){print 'Failed'; exit;}
+
+$returnXML=array('status'=>1);
+$rootName='session';
+require_once('../../scripts/http_end_options.php');
+exit;
+?>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
