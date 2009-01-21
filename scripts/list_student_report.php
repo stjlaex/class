8ff0ccbce7e1188491f83944f100299412e9df37
stjohnson <stj@laex.org>
2009-01-21 11:19:13 +0000

    list_student_report simplified

--- scripts/list_student_report.php
+++ scripts/list_student_report.php
@@ -5,56 +5,50 @@
  * list reports for which a student has assessments entered
  */
 
-	$todate=date('Y-m-d');
-	$d_rideid=mysql_query("SELECT DISTINCT report_id FROM rideid JOIN eidsid ON 
-				rideid.assessment_id=eidsid.assessment_id WHERE eidsid.student_id='$sid'
-			    ORDER BY report_id DESC");
-	$reports=array();
 	$reportwrappers=array();
-	while($rideid=mysql_fetch_array($d_rideid,MYSQL_ASSOC)){
-		$rid=$rideid['report_id'];
-		$d_report=mysql_query("SELECT id, title, course_id, date FROM report 
-			    WHERE id='$rid' ORDER BY date DESC, title");
-		while($report=mysql_fetch_array($d_report,MYSQL_ASSOC)){
-			$reports[$report['id']]=$report;
-			}
+
+	/* Reports with assessments */
+	$d_rideid=mysql_query("SELECT DISTINCT report_id FROM rideid JOIN eidsid ON 
+				rideid.assessment_id=eidsid.assessment_id WHERE eidsid.student_id='$sid';");
+	while($rep=mysql_fetch_array($d_rideid,MYSQL_ASSOC)){
+		$rid=$rep['report_id'];
 		$d_report=mysql_query("SELECT id, title, date FROM report JOIN
 				ridcatid ON ridcatid.report_id=report.id  
 			    WHERE ridcatid.categorydef_id='$rid' 
-				AND ridcatid.subject_id='wrapper' ORDER BY date DESC, title");
+				AND ridcatid.subject_id='wrapper';");
 		while($report=mysql_fetch_array($d_report,MYSQL_ASSOC)){
 			$reportwrappers[$report['id']]=$report;
 			}
 		}
+
+	/* Reports with comments but no assessments */
+	$d_repentry=mysql_query("SELECT DISTINCT report_id FROM reportentry WHERE
+				student_id='$sid';");
+	while($rep=mysql_fetch_array($d_repentry,MYSQL_ASSOC)){
+		$rid=$rep['report_id'];
+		$d_report=mysql_query("SELECT id, title, date FROM report JOIN
+				ridcatid ON ridcatid.report_id=report.id  
+			    WHERE ridcatid.categorydef_id='$rid' 
+				AND ridcatid.subject_id='wrapper' ORDER BY date DESC, title;");
+		while($report=mysql_fetch_array($d_report,MYSQL_ASSOC)){
+			$reportwrappers[$report['id']]=$report;
+			}
+		}
+
+	krsort($reportwrappers);
 ?>
 
 <div class="center"> 
   <label for="Current Reports"><?php print_string('reports');?></label>
   <select style="width:80%;" id="Current Reports" name="wrapper_rid"
 	  tabindex="<?php print $tab++;?>" size="18">
-	  <option value="">----<?php print_string('current');?>----------------</option>
 <?php
 	while(list($rid,$report)=each($reportwrappers)){
-		if($report['date']>=$todate){
 ?>
 		<option value="<?php print $report['id'];?>">
 			<?php print $report['title'].' ('.$report['date'].')';?>
 		</option>
 <?php
-			}
- 		}
-?>
-		<option value="">----<?php print_string('previous');?>----------------</option>
-<?php
-	reset($reportwrappers);
-	while(list($rid,$report)=each($reportwrappers)){
-		if($report['date']<$todate){
-?>
-		<option value="<?php print $report['id'];?>">
-			<?php print $report['title'].' ('.$report['date'].')';?>
-		</option>
-<?php
-			}
  		}
 ?>
   </select>
