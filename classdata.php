89f4965e1ce882457aca9c9915cf802cb15f8e9b
Stuart Johnson <stj@neveu.(none)>
2009-04-29 11:10:35 +0200

    move to git

--- classdata.php
+++ classdata.php
@@ -2,7 +2,7 @@
 require_once('include.php');
 $CFG->books=$books;
 $CFG->roles=$roles;
-$CFG->version='0.8.69';
+$CFG->version='0.9.1';
 $CFG->dirroot=$CFG->installpath.'/'.$CFG->applicationdirectory;
 global $CFG;
 $session='ClaSS'.$CFG->shortname;
