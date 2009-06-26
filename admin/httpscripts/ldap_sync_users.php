1678c61c0d0bfa00fe313c8d00bf945f95bbd88b
zrosado <zacarias.rosado@kingsgroup.org>
2009-06-26 14:29:05 +0200

    class to ldap enrolment

---
+++ admin/httpscripts/ldap_sync_users.php
@@ -0,0 +1,217 @@
+#! /usr/bin/php -q
+<?php
+/* ldap_sync_users.php
+ *
+ */
+ 
+/*
+ * head options: 
+ */ 
+$result=array();
+$error=array();
+
+echo (date("j F Y, H:i:s") . " - LDAP cron - ClaSS to LDAP. \n");
+
+require_once('/var/www/devclass/dbh_connect.php');
+require_once('/var/www/devclass/school.php');
+require_once('/var/www/devclass/classdev/classdata.php');
+require_once('/var/www/devclass/classdev/lib/include.php');
+require_once('/var/www/devclass/classdev/logbook/permissions.php');
+require_once('/var/www/devclass/classdev/lib/fetch_student.php');
+
+$db=db_connect();
+if (!$db) {
+	echo(date("j F Y, H:i:s") . " Couldn't connect to server. eop. \n");
+	die;
+}
+mysql_query("SET NAMES 'utf8'");
+
+/* 
+ * Core tasks: 
+ */
+
+/* Connect to LDAP server */
+$ds = ldap_connect($CFG->ldapserver);
+
+/* Make sure of right LDAP version is being used */
+ldap_set_option($ds, LDAP_OPT_PROTOCOL_VERSION, 3);
+
+if ($ds) {
+	/* Bind to LDAP DB */
+	$userrdn='cn='.$CFG->ldapuser.',dc='.$CFG->ldapdc1.',dc='.$CFG->ldapdc2;
+	$bind_result = ldap_bind($ds, $userrdn, $CFG->ldappasswd);
+
+	$info = array();
+	$row=array();
+
+	if ($bind_result) {
+
+/**
+ *	STEP 1: Process all users (teachers) from ClaSS
+ *	
+ */
+		$users = list_all_users('0');
+
+		/* process result */
+		foreach($users as $uid => $row) {
+
+		  $info = array();
+
+		  /* Search for entry in LDAP */
+		  $username=$row['username'];
+		  $classrole=$row['role'];
+		  $sr=ldap_search($ds, 'ou=people'.',dc='.$CFG->ldapdc1.',dc='.$CFG->ldapdc2, "uid=$username", $info);
+			
+		  if (ldap_count_entries($ds, $sr) > 0) {
+				/* When the entry exists, LDAP db is updated with values coming from ClaSS */
+				$info = ldap_first_entry($ds, $sr);				
+				$attrs = ldap_get_attributes($ds, $info);
+
+		    /* prepare data -in LDIF format- for LDAP insertion into DB */
+				$info = array();
+		    $info['uid'] = $row['username'];
+		    $info['userPassword'] = '{MD5}' . base64_encode(pack('H*',$row['passwd']));
+		    $info['cn'] = $row['forename'] . ' ' . $row['surname'];
+		    $info['givenName'] = $row['forename'];
+		    $info['sn'] = $row['surname'];
+		    $info['mail'] = $row['email'];
+		    $info['objectclass'] = 'inetOrgPerson';
+		    
+		    if ($attrs['employeeType'][0]<>$row['role']) {
+		    	/* change the LDAP entry to other superior RDN */
+		    	
+			    /* Read Entry again using detailed RDN, the old one */
+			    $distinguishedName = 'uid='.$username.',ou='.$attrs['employeeType'][0].',ou=people'.',dc='.$CFG->ldapdc1.',dc='.$CFG->ldapdc2;
+			    $sr=ldap_search($ds, $distinguishedName, "uid=$username");
+			    /* change type: modify */
+			    if ($sr) {
+				    $info['employeeType'] = $row['role'];
+						$info['ou'] = $row['role'];
+			    	$mod=ldap_modify ( $ds, $distinguishedName , $info );
+			    	$full_old_dn= $distinguishedName;
+						$new_rdn= 'uid='.$username;
+						$new_superior='ou='.$info['employeeType'].',ou=people'.',dc='.$CFG->ldapdc1.',dc='.$CFG->ldapdc2;
+						$ldren=ldap_rename( $ds, $full_old_dn, $new_rdn, $new_superior, TRUE);					
+			    }
+		    } else {
+				  /* modify ldap entry */
+					$distinguishedName = 'uid='.$username.',ou='.$row['role'].',ou=people'.',dc='.$CFG->ldapdc1.',dc='.$CFG->ldapdc2;
+				  $r = ldap_modify($ds, $distinguishedName, $info);
+				  if (!$r) {
+				  	trigger_error('Unable to modify entry in LDAP DB: '.$distinguishedName, E_USER_WARNING);
+				  }
+		    }
+		  } else {
+				/* OK, the entry does not exist in LDAP so insert it into LDAP DB	 */
+				$info = array();
+		    /* prepare data -in LDIF format- for LDAP insertion into DB */
+		    $info['uid'] = $row['username'];
+		    $info['userPassword'] = '{MD5}' . base64_encode(pack('H*',$row['passwd']));
+		    $info['cn'] = $row['forename'] . ' ' . $row['surname'];
+		    $info['givenName'] = $row['forename'];
+		    $info['sn'] = $row['surname'];
+		    $info['mail'] = $row['email'];
+		    $info['objectclass'] = 'inetOrgPerson';
+		    $info['ou']	= $row['role'];
+		    $info['employeeType'] = $row['role'];
+		    /* add data to ldap directory */
+		    $distinguishedName = 'uid='.$username.',ou='.$info['employeeType'].',ou=people'.',dc='.$CFG->ldapdc1.',dc='.$CFG->ldapdc2;
+		    $r = ldap_add($ds, $distinguishedName, $info);
+		    if (!$r) {
+				  trigger_error('Unable to insert entry into LDAP DB: '.$distinguishedName, E_USER_WARNING);
+		    }
+		  } 
+		}
+
+/**
+ * STEP 2: Process all Students from ClaSS DB
+ *
+ */
+
+	$yearcoms=(array)list_communities('year');
+
+	$Students=array();
+
+	while(list($yearindex,$com)=each($yearcoms)){
+		$yid=$com['name'];
+		$students=listin_community($com);
+		
+		while(list($studentindex,$student)=each($students)){
+			$sid=$student['id'];
+			$Students[$sid]=fetchStudent_short($sid);
+			$Email=fetchStudent_singlefield($sid,'EmailAddress');
+			$EnrolNumber=fetchStudent_singlefield($sid,'EnrolNumber');
+			$EPFUsername=fetchStudent_singlefield($sid,'EPFUsername');
+			$Students[$sid]['EmailAddress']['value']=$Email['EmailAddress']['value'];
+			$Students[$sid]['EPFUsername']['value']=$EPFUsername['EPFUsername']['value'];
+	
+	    /* Search for entry in LDAP */
+			//$username=$Students[$sid]['EPFUsername']['value'];
+			$username=get_epfusername($student['id']);
+
+	    $sr=ldap_search($ds, 'ou=students'.',ou=people'.',dc='.$CFG->ldapdc1.',dc='.$CFG->ldapdc2, "uid=$username");
+
+			$entry=array();
+			$info=array();
+
+	    if (ldap_count_entries($ds, $sr) > 0) {
+
+				/* When the entry exists, LDAP db is updated with values coming from ClaSS */
+				$entry = ldap_first_entry($ds, $sr);
+				$attrs = ldap_get_attributes($ds, $entry);
+				for ($i=0; $i < $attrs['count']; $i++) {
+						$values = ldap_get_values($ds, $entry, $attrs[$i]);
+				}
+		    /* prepare data -in LDIF format- for LDAP field replacement */
+				$info=array();
+		    $info['uid'] 					= $username;
+		    $info['userPassword'] = '{MD5}' . base64_encode(pack('H*',md5('abc123')));
+		    $info['cn'] 					= $Students[$sid]['Forename']['value'] . ' ' . $Students[$sid]['Surname']['value'];
+		    $info['givenName']		= $Students[$sid]['Forename']['value'];
+		    $info['sn'] 					= $Students[$sid]['Surname']['value'];
+		    $info['mail'] 				= $Students[$sid]['EmailAddress']['value'];
+				$info['ou']						= 'Students'; 
+		    $info['objectclass'] 	= 'inetOrgPerson';
+		    /* add data to ldap directory */
+		    $distinguishedName = "uid=$username" . ',ou=students'.',ou=people'.',dc='.$CFG->ldapdc1.',dc='.$CFG->ldapdc2;
+		    $r = ldap_modify($ds, $distinguishedName, $info);
+		    if (!$r) {
+		    	trigger_error('Unable to modify LDAP DB entry', E_USER_WARNING);
+		    }
+					
+			} else {
+					/* OK, the entry does not exist in LDAP so insert it into LDAP DB */
+					$info=array();
+			    /* prepare data -in LDIF format- for LDAP insertion into DB */
+			    $info['uid'] 					= $username;
+			    $info['userPassword'] = '{MD5}' . base64_encode(pack('H*',md5('abc123')));
+			    $info['cn'] 					= $Students[$sid]['Forename']['value'] . ' ' . $Students[$sid]['Surname']['value'];
+			    $info['givenName']		= $Students[$sid]['Forename']['value'];
+			    $info['sn'] 					= $Students[$sid]['Surname']['value'];
+			    $info['mail'] 				= $Students[$sid]['EmailAddress']['value'];
+					$info['ou']						= 'Students'; 
+			    $info['objectclass'] 	= 'inetOrgPerson';
+			    /* add data to ldap directory */
+			    $distinguishedName = "uid=$username" . ',ou=students'.',ou=people'.',dc='.$CFG->ldapdc1.',dc='.$CFG->ldapdc2;
+			    $r = ldap_add($ds, $distinguishedName, $info);
+			    if (!$r) {
+			    	trigger_error('Unable to insert entry into LDAP DB: '.$distinguishedName, E_USER_WARNING);
+			    }
+		    } 
+			} 			
+		}
+  ldap_close($ds);
+	} else {
+	  trigger_error('Unable to bind to LDAP server. Nothing has been done.', E_USER_WARNING);
+		echo '---> eop. $bind_result: '.$bind_result.'<br />';
+	}
+} else {
+  trigger_error('Unable to connect to LDAP server. Nothing has been done.', E_USER_WARNING);
+} 
+
+/* 
+ * End options 
+ */
+echo (date("j F Y, H:i:s") . " - LDAP cron - ClaSS to LDAP - eop\n");
+
+?>
