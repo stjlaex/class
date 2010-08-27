bb352ac60a27bfc450b2963731468876f54e91f5
stjohnson <stj@laex.org>
2010-08-27 17:21:26 +0200

    spellchecker for tinymce

--- js/bookfunctions.js
+++ js/bookfunctions.js
@@ -820,13 +820,13 @@ function loadRequired(){
 				  tinyMCE.init({
 					mode : "textareas",
 					theme : "advanced",
-					theme_advanced_buttons1 : "bold,italic,underline,separator,justifyleft,justifycenter,justifyright,justifyfull,separator,bullist,numlist,undo,redo", 
+					  theme_advanced_buttons1 : "bold,italic,underline,separator,justifyleft,justifycenter,justifyright,justifyfull,separator,bullist,numlist,undo,redo,spellchecker", 
 					theme_advanced_buttons2 : "",
 					theme_advanced_buttons3 : "",
 					theme_advanced_toolbar_location : "top",
 					theme_advanced_toolbar_align : "left",
 					theme_advanced_statusbar_location : "bottom",
-					plugins : 'inlinepopups'
+					  plugins : 'inlinepopups,spellchecker'
 					});
 				  }
 
