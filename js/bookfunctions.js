836e4b384b68ee9baab91e80bc96170911ab9c60
stjohnson <stj@laex.org>
2010-10-04 12:27:19 +0200

    past plugin for tinyMCE

--- js/bookfunctions.js
+++ js/bookfunctions.js
@@ -822,13 +822,15 @@ function loadRequired(){
 				  tinyMCE.init({
 					mode : "textareas",
 					theme : "advanced",
-					  theme_advanced_buttons1 : "bold,italic,underline,separator,justifyleft,justifycenter,justifyright,justifyfull,separator,bullist,numlist,undo,redo,spellchecker", 
+					  theme_advanced_buttons1 : "bold,italic,underline,separator,justifyleft,justifycenter,justifyright,justifyfull,separator,bullist,numlist,undo,redo,spellchecker,pastetext,pasteword,selectall", 
 					theme_advanced_buttons2 : "",
 					theme_advanced_buttons3 : "",
 					theme_advanced_toolbar_location : "top",
 					theme_advanced_toolbar_align : "left",
 					theme_advanced_statusbar_location : "bottom",
-					  plugins : 'inlinepopups,spellchecker'
+					  plugins : 'inlinepopups,spellchecker,paste',
+					  paste_auto_cleanup_on_paste : true,
+					  paste_retain_style_properties : ''
 					});
 				  }
 
