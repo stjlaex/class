b190fe8c90951f10b6654638a1c07d9fc716ded9
stjohnson <stj@laex.org>
2011-05-02 11:48:24 +0200

    new attendance booking table

--- install/patch-0.9.40.sql
+++ install/patch-0.9.40.sql
@@ -3,6 +3,7 @@ CREATE TABLE attendance_booking (
 	student_id		int unsigned not null default '0',
 	community_id	int unsigned not null default '0',
 	day				enum('1','2','3','4','5','6','7','%') not null default '%',
+	session			enum('AM','PM') not null default 'AM',
 	status			enum('a','p') not null default 'p',
 	code			char(2) not null default '',
 	startdate		date not null default '0000-00-00',
