e4736cc51e4c6b76e8544199a40bdc50c232359a
stjohnson <stj@laex.org>
2011-12-28 12:22:51 +0100

    transport

---
+++ admin/httpscripts/transport_route_print.php
@@ -0,0 +1,64 @@
+<?php
+/**									transport_route_print.php
+ *
+ */
+
+require_once('../../scripts/http_head_options.php');
+
+/*NB. The busnames are pulled by checksidsAction hence they honestly really are called sids!*/
+if(isset($_GET['sids'])){$busnames=(array)$_GET['sids'];}else{$busnames=array();}
+if(isset($_POST['sids'])){$busnames=(array)$_POST['sids'];}
+
+
+$buses=list_buses();
+
+if(sizeof($busnames)==0){
+	$result[]=get_string('youneedtoselectstudents');
+	$returnXML=$result;
+	$rootName='Error';
+	}
+else{
+
+	$Students=array();
+	$Students['Transport']=array();
+
+	foreach($busnames as $typebusname){
+		list($type,$busname)=explode('-',$typebusname);
+		if($busname!=''){
+			$Transport=array();
+			$Transport['Name']=array('value'=>$busname);
+			$directions=array('I','O');
+			foreach($directions as $direction){
+				$bus=(array)get_bus('',$busname,$direction,'%');
+				$stops=list_bus_stops($bus['id']);
+				$Journey=array();
+				$Journey['Direction']=$bus['direction'];
+				$Journey['Bus']=array('id_db'=>$bus['id'],
+									  'value'=>$busname,
+									  'time'=>$bus['departuretime']
+									  );
+				$Stops=array();
+				$Stops['Stop']=array();
+				foreach($stops as $stop){
+					$Stops['Stop'][]=array('id_db'=>$stop['id'],
+										   'sequence'=>$stop['sequence'],
+										   'name'=>$stop['name'],
+										   'detail'=>$stop['detail'],
+										   'time'=>$stop['traveltime']
+										   );
+					}
+				$Journey['Stops']=$Stops;
+				$Transport['Journey'][]=$Journey;
+				}
+			$Students['Transport'][]=$Transport;
+			}
+		}
+
+
+	$returnXML=$Students;
+	$rootName='Students';
+	}
+
+require_once('../../scripts/http_end_options.php');
+exit;
+?>
