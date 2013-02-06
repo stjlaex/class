6b1d07e87cb7e91fdb7dcb185906578844a1a612
stjohnson <stj@laex.org>
2013-02-06 15:47:24 +0100

    d3 js added

--- js/book.js
+++ js/book.js
@@ -1205,7 +1205,7 @@ function openChartReport(xml, xsltName, paper){
 	printWindow.document.writeln("<link rel='stylesheet' type='text/css' href='../templates/"+xsltName+".css' media='all' title='ReportBook Output' />");
 	printWindow.document.writeln("<script language='JavaScript' type='text/javascript' src='js/raphael.js' charset='utf-8'></script>");
 	printWindow.document.writeln("<script language='JavaScript' type='text/javascript' src='js/g.raphael-min.js' charset='utf-8'></script>");
-	printWindow.document.writeln("<script language='JavaScript' type='text/javascript' src='http://d3js.org/d3.v3.min.js' charset='utf-8'></script>");
+	printWindow.document.writeln("<script language='JavaScript' type='text/javascript' src='js/d3/d3.v3.min.js' charset='utf-8'></script>");
 	printWindow.document.writeln("<script language='JavaScript' type='text/javascript' src='../templates/"+xsltName+".js' charset='utf-8'></script>");
 	printWindow.document.writeln("<meta http-equiv='pragma' content='no-cache'/>");
 	printWindow.document.writeln("<meta http-equiv='Expires' content='0'/>");
