902d2f3f512fd81a6ed43fb6ad0bfb0094059fe1
Sidimar <sidimar@learningdata.ie>
2014-01-29 15:56:20 +0000

    Updated header after logged

--- index.php
+++ index.php
@@ -1,112 +1,106 @@
 <?php
-/**
- *
- * ClaSS is the ClaSS Student System, a complete student
- * tracking, reporting, and information management system for schools.
- *
- * Copyright (C) 2002-2012 by Stuart Thomas Johnson.
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Affero General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU Affero General Public License
- * along with this program. If not, see <http://www.gnu.org/licenses/agpl.html>.
- *
- * @package    class
- * @subpackage core
- * @author     Stuart Thomas Johnson
- * @license    http://www.gnu.org/licenses/agpl.html GNU AGPL
- * @copyright  (C) 2012 Stuart Thomas Johnson
- *
- */
-require_once ('../school.php');
-require_once ('classdata.php');
-require_once ('lib/include.php');
-/* Just maybe last logout wasn't clean... */
-require_once ('logbook/session.php');
-start_class_phpsession();
-kill_class_phpsession();
-$books = $CFG -> books;
-$currentlang = current_language();
-print '<?xml version="1.0" encoding="utf-8"?' . '>';
-?>
+  /**
+   *
+   * ClaSS is the ClaSS Student System, a complete student
+   * tracking, reporting, and information management system for schools.
+   *
+   * Copyright (C) 2002-2012 by Stuart Thomas Johnson.
+   *
+   * This program is free software: you can redistribute it and/or modify
+   * it under the terms of the GNU Affero General Public License as published by
+   * the Free Software Foundation, either version 3 of the License, or
+   * (at your option) any later version.
+   *
+   * This program is distributed in the hope that it will be useful,
+   * but WITHOUT ANY WARRANTY; without even the implied warranty of
+   * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+   * GNU General Public License for more details.
+   *
+   * You should have received a copy of the GNU Affero General Public License
+   * along with this program. If not, see <http://www.gnu.org/licenses/agpl.html>.
+   *
+   * @package    class
+   * @subpackage core
+   * @author     Stuart Thomas Johnson
+   * @license    http://www.gnu.org/licenses/agpl.html GNU AGPL
+   * @copyright  (C) 2012 Stuart Thomas Johnson
+   *
+   */
+  require_once ('../school.php');
+  require_once ('classdata.php');
+  require_once ('lib/include.php');
+  /* Just maybe last logout wasn't clean... */
+  require_once ('logbook/session.php');
+  start_class_phpsession();
+  kill_class_phpsession();
+  $books = $CFG -> books;
+  $currentlang = current_language();
+  print '<?xml version="1.0" encoding="utf-8"?' . '>';
+    ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" lang="<?php print $currentlang; ?>" xml:lang="<?php print $currentlang; ?>">
-<head profile="http://www.w3.org/2005/10/profile">
-<title><?php print $CFG -> sitename; ?></title>
-<link rel="icon" type="image/png" href="images/classicon.png" />
-<meta http-equiv="content-type" content="application/xhtml+xml; charset=utf-8" />
-<meta http-equiv="Content-Script-Type" content="text/JavaScript" />
-<meta name="copyright" content="Copyright 2002-2012 Stuart Thomas Johnson. All trademarks acknowledged. All rights reserved." />
-<meta name="version" content="<?php print $CFG -> version; ?>" />
-<meta name="license" content="GNU Affero General Public License version 3" />
-<link href="css/hoststyle.css" rel="stylesheet" type="text/css" />
-<link href="css/selery.css?version=1042" rel="stylesheet" type="text/css" />
-<link rel="stylesheet" href="css/uniform.default.css" media="screen" />
-<link rel="stylesheet" href="css/uniform.edit.css" media="screen" />
-
-</head>
-
-<body onload="loadLogin('logbook.php');">
-
-<header>
-    <div class="container">
+  <head profile="http://www.w3.org/2005/10/profile">
+    <title><?php print $CFG -> sitename; ?></title>
+    <link rel="icon" type="image/png" href="images/classicon.png" />
+    <meta http-equiv="content-type" content="application/xhtml+xml; charset=utf-8" />
+    <meta http-equiv="Content-Script-Type" content="text/JavaScript" />
+    <meta name="copyright" content="Copyright 2002-2012 Stuart Thomas Johnson. All trademarks acknowledged. All rights reserved." />
+    <meta name="version" content="<?php print $CFG -> version; ?>" />
+    <meta name="license" content="GNU Affero General Public License version 3" />
+    <link href="css/selery.css?version=1042" rel="stylesheet" type="text/css" />
+    <link href='http://fonts.googleapis.com/css?family=Lato:100,300,400,700,900' rel='stylesheet' type='text/css'>
+    <link href="//netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css" rel="stylesheet">
+    <link rel="stylesheet" href="css/uniform.default.css" media="screen" />
+    <link rel="stylesheet" href="css/uniform.edit.css" media="screen" />
+    <link href="css/hoststyle.css" rel="stylesheet" type="text/css" />
+  </head>
+  <body onload="loadLogin('logbook.php');">
+    <header>
+      <div class="container">
         <div id="navtabs">
-            <div class="booktabs"></div>
+          <div class="booktabs"></div>
         </div>
         <div id="branded" class="branded">
-            <img src="../images/logo.png" />
+          <img src="../images/logo.png" />
         </div>
         <div id="logbook">
-            <form id="langchoice" name="langpref" method="post" action="logbook.php" target="viewlogbook">
-            </form>
+          <form id="langchoice" name="langpref" method="post" action="logbook.php" target="viewlogbook">
+          </form>
         </div>
-    </div>
-</header>
-
-
-<iframe id="viewlogbook" name="viewlogbook" class="coverframe" scrolling="no"></iframe>
-<div id="logbookoptions" class="bookoptions"></div>
-
-<div id="aboutbookoptions" class="bookoptions"></div>
-<iframe id="viewaboutbook" name="viewaboutbook" class="bookframe"></iframe>
-
-<?php
-    /* Use all because it contains all possible books*/
-    /* even if after login user does not have access*/
-    $showbooks=$books['all'];
-    foreach($showbooks as $bookhost=>$bookname){
-?>
-        <div id="<?php print $bookhost . 'options'; ?>" class="bookoptions"></div>
-        <iframe id="<?php print 'view' . $bookhost; ?>" name="<?php print 'view' . $bookhost; ?>" class="bookframe"></iframe>
-<?php
-}
-?>
-
-<?php
-    $showbooks=$books['external']['all'];
-    foreach($showbooks as $bookhost=>$bookname){
-?>
-        <div id="<?php print $bookhost . 'options'; ?>" style="display:none;" class="bookoptions"></div>
-        <iframe id="<?php print 'view' . $bookhost; ?>" name="<?php print 'view' . $bookhost; ?>" class="bookframe"></iframe>
-<?php
-}
-?>
+      </div>
+    </header>
+    <iframe id="viewlogbook" name="viewlogbook" class="coverframe" scrolling="no"></iframe>
+    <div id="logbookoptions" class="bookoptions"></div>
+    <div id="aboutbookoptions" class="bookoptions"></div>
+    <iframe id="viewaboutbook" name="viewaboutbook" class="bookframe"></iframe>
+    <?php
+      /* Use all because it contains all possible books*/
+      /* even if after login user does not have access*/
+      $showbooks=$books['all'];
+      foreach($showbooks as $bookhost=>$bookname){
+      ?>
+    <div id="<?php print $bookhost . 'options'; ?>" class="bookoptions"></div>
+    <iframe id="<?php print 'view' . $bookhost; ?>" name="<?php print 'view' . $bookhost; ?>" class="bookframe"></iframe>
+    <?php
+      }
+      ?>
+    <?php
+      $showbooks=$books['external']['all'];
+      foreach($showbooks as $bookhost=>$bookname){
+      ?>
+    <div id="<?php print $bookhost . 'options'; ?>" style="display:none;" class="bookoptions"></div>
+    <iframe id="<?php print 'view' . $bookhost; ?>" name="<?php print 'view' . $bookhost; ?>" class="bookframe"></iframe>
+    <?php
+      }
+      ?>
     <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.8/jquery.min.js"></script>
     <script src="js/host.js?version=1042"></script>
     <script src="js/jquery.uniform.min.js"></script>
     <script>
-        $("iframe").load(function() {
-            $("select").uniform();
-            $("iframe").contents().find("select").uniform();
-        });
+      $("iframe").load(function() {
+          $("select").uniform();
+          $("iframe").contents().find("select").uniform();
+      });
     </script>
-</body>
+  </body>
 </html>
--- logbook.php
+++ logbook.php
@@ -1,160 +1,142 @@
 <?php
-$host = 'logbook.php';
-$book = 'logbook';
-$fresh = '';
-include ('scripts/head_options.php');
-if (isset($_POST['new_r'])) {$_SESSION['r'] = $_POST['new_r'];
-    $fresh = 'yes';
-}
-if (!isset($_SESSION['r'])) {$_SESSION['r'] = -1;
-    $fresh = 'very';
-}
+    $host='logbook.php';
+    $book='logbook';
+    $fresh='';
+    include('scripts/head_options.php');
+    if(isset($_POST['new_r'])){$_SESSION['r']=$_POST['new_r'];$fresh='yes';}
+    if(!isset($_SESSION['r'])){$_SESSION['r']=-1;$fresh='very';}
 ?>
-  <div style="visibility:hidden;" id="hiddenbookoptions">	
-  </div>
+  <div style="visibility:hidden;" id="hiddenbookoptions"></div>
 
   <div style="visibility:hidden;" id="hiddenloginlabel">
-	<?php print $tid; ?>
+    <?php print $tid;?>
   </div>
 
   <div style="visibility:hidden;" id="hiddenlogbook">
-	<div id="logbookstripe" class="logbook"></div>
-	<div id="loginworking">
-	  <form  id="loginchoice" name="workingas" method="post" 
-						action="logbook.php" target="viewlogbook">
-	  <select name="new_r" size="1" onChange="document.workingas.submit();">
-		<option value="-1" <?php
-        if ($r == -1) {print 'selected="selected" ';
-        }
- ?>
-		  ><?php print_string('myclasses'); ?></option>
+    <!--div id="logbookstripe" class="logbook"></div-->
+    <!--div id="sidebuttons" class="sidebuttons">
+      <button onclick="viewBook('aboutbook');" title="<?php print_string('about');?>"><img src="images/help-browser.png" /></button>
+      <button id="sitestatus" class="hide" ><img src="images/roller.gif"/></button>
+      <button id="siteicon" class="show" onClick="loadBook('');" title="<?php print_string('reload');?>" ><img src="images/view-refresh.png" alt="<?php print_string('reload');?>" /></button>
+      <button onClick="printGenericContent();" title="<?php print_string('print');?>"><img src="images/printer.png" alt="<?php print_string('print');?>" /></button>
+    </div-->
+  </div>
+
 <?php
-            foreach ($respons as $rindex => $respon) {
-                /* Lists the academic responsibilities. */
-                print '<option value="' . $rindex . '"';
-                if (isset($r) and $r == $rindex) {print ' selected="selected" ';
+    if($fresh!=''){
+        $role=$_SESSION['role'];
+        if($_SESSION['senrole']=='1'){$books[$role]['seneeds']='Support';}
+        if($_SESSION['medrole']=='1'){$books[$role]['medbook']='Medical';}
+        }
+
+    if($fresh=='yes'){
+        /* Responsibilities selection has changed 
+         * (re)loading all the $r dependent ClaSS books.
+         */
+        foreach($books[$role] as $bookhost=>$bookname){
+            if($bookhost=='markbook' or $bookhost=='reportbook'
+               or $bookhost=='admin'){
+?>
+                <script>parent.loadBook("<?php print $bookhost; ?>")</script>
+<?php
                 }
-                print '>' . $respon['name'] . '</option>';
+            if($bookhost=='markbook'){
+                /* Clear everything because the user's current
+                 * selection will no longer be avaible. 
+                 */
+                unset($_SESSION['classes']);
+                unset($_SESSION['cids']);
+                unset($_SESSION['cid']);
+                unset($_SESSION['pids']);
+                unset($_SESSION['pid']);
+                unset($_SESSION['umntype']);
+                unset($_SESSION['umnrank']);
+                unset($_SESSION['umns']);
+                unset($_SESSION['viewtable']);
+                }
+           }
+        }
+    elseif($fresh=='very'){
+        /* This was loaded after a new login so do some extra stuff:
+         * load the externalbooks, booktabs, update langpref, and raise firstbook
+         */
+
+        $firstbookpref=$_SESSION['firstbookpref'];
+
+        if($role=='office' or $role=='medical' or $role=='admin' or $role=='library'){
+            /* This will prevent session timeouts, making an
+             * xmlhttprequest to the logbook/httpscripts/session_alive.php 
+             * every 15 minutes. But only for select roles.
+             */
+?>
+        <script>setInterval("parent.sessionAlive(pathtobook);",15*60*1000);</script>
+<?php
+            if($tid=='administrator'){
+                check_class_release();
+                }
+            }
+
+        foreach($books[$role] as $bookhost=>$bookname){
+?>
+                <script>parent.loadBook("<?php print $bookhost; ?>")</script>
+<?php
+           }
+
+        $externalbooks=array();
+        if(isset($books['external'][$role])){$externalbooks[$role]=$books['external'][$role];}
+        else{$externalbooks[$role]=array();}
+        foreach($externalbooks[$role] as $bookhost=>$bookname){
+            /*loading all the external books - only needed once*/
+?>
+            <script>parent.loadBook("<?php print $bookhost; ?>")</script>
+<?php
+           }
+
+        $showtabs=$books[$role]+$externalbooks[$role];
+?>
+
+        <script>parent.loadBook("aboutbook")</script>
+        <script>tabtimer=setTimeout("parent.viewBook('<?php print $firstbookpref; ?>');",1000);</script>
+
+    <div style="visibility:hidden;" id="hiddennavtabs">
+        <div class="booktabs">
+            <div class="user-logout">
+                <label id="loginlabel"></label> | 
+                <a id="logbooktab" class="logbook" onclick="logOut();"><span class="fa fa-power-off"></span></a>
+            </div>
+            <div id="loginworking">
+                <form  id="loginchoice" name="workingas" method="post" action="logbook.php" target="viewlogbook">
+                    <select name="new_r" size="1" onChange="document.workingas.submit();">
+                        <option value="-1" <?php  if($r==-1){print 'selected="selected" ';} ?>><?php print_string('myclasses');?></option>
+                        <?php 
+                            foreach($respons as $rindex => $respon){
+                                /* Lists the academic responsibilities. */
+                                print '<option value="'.$rindex.'"';
+                                if(isset($r) and $r==$rindex){print ' selected="selected" ';}
+                                print '>'.$respon['name'].'</option>';
+                            }
+                        ?>
+                    </select>
+              </form>
+            </div>
+            <ul>
+                <li id="aboutbooktab" style="display:none;"><a id="currentbook" class="aboutbook">About</a></li>
+                    <?php
+                        foreach($showtabs as $bookhost=>$bookname){
+                    ?>
+                <li id="<?php print $bookhost.'tab';?>"><a class="<?php print $bookhost;?>" onclick="viewBook(this.getAttribute('class'));"><?php print $bookname;?></a></li>
+                    <?php
+                        }
+                    ?>
+            </ul>
+        </div>
+    </div>
+        <?php
+            update_user_language(current_language());
+        ?>
+        <script>parent.logInSuccess();</script>
+        <?php
             }
         ?>
-		</select>
-	  </form>
-	</div>
-
-	<div id="sidebuttons" class="sidebuttons">
-	  <button onclick="viewBook('aboutbook');" title="<?php print_string('about'); ?>">
-		<img src="images/help-browser.png" /></button>
-	  <button id="sitestatus" class="hide" >
-		<img src="images/roller.gif"/></button>
-	  <button id="siteicon" class="show" onClick="loadBook('');" 
-		title="<?php print_string('reload'); ?>" >
-		<img src="images/view-refresh.png" alt="<?php print_string('reload'); ?>" /></button>
-	  <button onClick="printGenericContent();" title="<?php print_string('print'); ?>">
-		<img src="images/printer.png" alt="<?php print_string('print'); ?>" /></button>
-	</div>
-
-  </div>
-
-<?php
-	if($fresh!=''){
-		$role=$_SESSION['role'];
-		if($_SESSION['senrole']=='1'){$books[$role]['seneeds']='Support';}
-		if($_SESSION['medrole']=='1'){$books[$role]['medbook']='Medical';}
-		}
-
-	if($fresh=='yes'){
-		/* Responsibilities selection has changed 
-		 * (re)loading all the $r dependent ClaSS books.
-		 */
-		foreach($books[$role] as $bookhost=>$bookname){
-			if($bookhost=='markbook' or $bookhost=='reportbook'
-			   or $bookhost=='admin'){
-?>
-				<script>parent.loadBook("<?php print $bookhost; ?>")</script>
-<?php
-}
-if($bookhost=='markbook'){
-/* Clear everything because the user's current
-* selection will no longer be avaible.
-*/
-unset($_SESSION['classes']);
-unset($_SESSION['cids']);
-unset($_SESSION['cid']);
-unset($_SESSION['pids']);
-unset($_SESSION['pid']);
-unset($_SESSION['umntype']);
-unset($_SESSION['umnrank']);
-unset($_SESSION['umns']);
-unset($_SESSION['viewtable']);
-}
-}
-}
-elseif($fresh=='very'){
-/* This was loaded after a new login so do some extra stuff:
-* load the externalbooks, booktabs, update langpref, and raise firstbook
-*/
-
-$firstbookpref=$_SESSION['firstbookpref'];
-
-if($role=='office' or $role=='medical' or $role=='admin' or $role=='library'){
-/* This will prevent session timeouts, making an
-* xmlhttprequest to the logbook/httpscripts/session_alive.php
-* every 15 minutes. But only for select roles.
-*/
-?>
-		<script>setInterval("parent.sessionAlive(pathtobook);", 15 * 60 * 1000);</script>
-<?php
-if ($tid == 'administrator') {
-    check_class_release();
-}
-}
-
-foreach($books[$role] as $bookhost=>$bookname){
-?>
-				<script>parent.loadBook("<?php print $bookhost; ?>")</script>
-<?php
-}
-
-$externalbooks=array();
-if(isset($books['external'][$role])){$externalbooks[$role]=$books['external'][$role];}
-else{$externalbooks[$role]=array();}
-foreach($externalbooks[$role] as $bookhost=>$bookname){
-/*loading all the external books - only needed once*/
-?>
-			<script>parent.loadBook("<?php print $bookhost; ?>")</script>
-<?php
-}
-
-$showtabs=$books[$role]+$externalbooks[$role];
-?>
-
-		<script>parent.loadBook("aboutbook")</script>
-		<script>tabtimer=setTimeout("parent.viewBook('<?php print $firstbookpref; ?>');",1000);</script>
-
-  <div style="visibility:hidden;" id="hiddennavtabs">
-	<div class="booktabs">
-	  <ul>
-		<label id="loginlabel">
-		</label>
-		<li id="logbooktab"><a class="logbook" onclick="logOut();"><span class="fa fa-power-off"></span></a></li>
-		<li id="aboutbooktab" style="display:none;"><p id="currentbook" class="aboutbook">About</p>
-		</li>
-<?php
-		foreach($showtabs as $bookhost=>$bookname){
-?>
-		<li id="<?php print $bookhost . 'tab'; ?>"><a class="<?php print $bookhost; ?>" onclick="viewBook(this.getAttribute('class'));"><?php print $bookname; ?></a></li>
-<?php
-}
-?>
-	  </ul>
-	</div>
-  </div>
-<?php
-update_user_language(current_language());
-?>
-		<script>parent.logInSuccess();</script>
-<?php
-}
-?>
 </body>
 </html>
