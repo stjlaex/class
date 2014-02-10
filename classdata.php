b36d47bcf3af74733987f9bf5284d546e20d1740
Stuart Johnson <stj@laex.org>
2014-02-10 16:05:35 +0000

    release 1.0.62

--- classdata.php
+++ classdata.php
@@ -2,7 +2,7 @@
 require_once('include.php');
 $CFG->books=$books;
 $CFG->roles=$roles;
-$CFG->version='1.0.62';
+$CFG->version='1.0.63';
 $CFG->dirroot=$CFG->installpath.'/'.$CFG->applicationdirectory;
 global $CFG;
 $session='ClaSS'.$CFG->shortname;
