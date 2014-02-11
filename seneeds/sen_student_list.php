a8a2e337ca1ea2268b97c8b76e74507374a3bf4b
Marius Avram <marius@learningdata.ie>
2014-02-11 13:43:26 +0000

    sorting added for sen and medbook student lists closes #21

--- seneeds/sen_student_list.php
+++ seneeds/sen_student_list.php
@@ -90,22 +90,57 @@ if(sizeof($sids)>0){
 	two_buttonmenu($extrabuttons,'infobook');
 ?>
 
-	<div id="viewcontent" class="content">
-	  <form id="formtoprocess" name="formtoprocess" method="post" action="<?php print $host;?>">
-		<table class="listmenu sidtable">
-		  <th colspan="2"><?php print_string('checkall'); ?><input type="checkbox" name="checkall" 
-				value="yes" onChange="checkAll(this);" /></th>
-		  <th><?php print_string('student'); ?></th>
-		  <th><?php print_string('formgroup'); ?></th>
+<div id="viewcontent" class="content">
+	<form id="formtoprocess" name="formtoprocess" method="post" action="<?php print $host;?>">
+		<table class="listmenu sidtable" id="sidtable">
+			<thead>
+				<tr>
+					<th rowspan="2" colspan="2">
+						<?php print_string('checkall'); ?>
+						<input type="checkbox" name="checkall" value="yes" onChange="checkAll(this);" />
+					</th>
+					<th rowspan="2" style="width:20%;">
+						<?php print_string('student'); ?>
+					</th>
+					<th rowspan="2" style="width:10%;">
+						<?php print_string('formgroup'); ?>
+					</th>
 <?php
 	$extra_studentfields=array('NextReviewDate'=>'nextreviewdate');
-	foreach($displayfields as $displayfield){
+	foreach($displayfields as $dno => $displayfield){
 ?>
-		<th style="width:<?php print $displayfields_width;?>;">
-		<?php include('scripts/list_studentfield.php');?>
-		</th>
+					<th style="width:<?php print $displayfields_width;?>;">
+					<?php include('scripts/list_studentfield.php');?>
+					</th>
 <?php
 		}
+?>
+				</tr>
+				<tr>
+<?php
+		foreach($displayfields as $dno => $displayfield){
+		$sortno=$dno+4;
+		//a=age, d=date, i=integer, s=string
+		if($displayfield=='Age'){$sort_types.=",'a'";}
+		elseif($displayfield=='DOB'  or $displayfield=='IdExpiryDate' or $displayfield=='LeavingDate' or $displayfield=='EntryDate' or $displayfield=='EnrolmentApplicationDate'){$sort_types.=",'d'";}
+		elseif($displayfield=='YearGroup' or $displayfield=='EnrolmentYearGroup' or $displayfield=='EnrolNumber'){$sort_types.=",'i'";}
+		else{$sort_types.=",'s'";}
+?>
+					<th class="noprint">
+						<div class="rowaction">
+							<input class="underrow" type='button' name='action' value='v' onClick='tsDraw("<?php print $sortno;?>A", "sidtable");' />
+							<input class="underrow"  type='button' name='action' value='-' onClick='tsDraw("<?php print $sortno;?>U", "sidtable");' />
+							<input class="underrow"  type='button' name='action' value='^' onClick='tsDraw("<?php print $sortno;?>D", "sidtable");' />
+						</div>
+					</th>
+		
+<?php
+		}
+?>
+				</tr>
+			</thead>
+			<tbody>
+<?php
 
 	foreach($sids as $sindex => $sid){
 		$display='yes';
@@ -125,26 +160,26 @@ if(sizeof($sids)>0){
 			}
 		if($display=='yes'){
 ?>
-		<tr>
-		  <td>
-			<input type="checkbox" name="sids[]" value="<?php print $sid; ?>" />
-			<?php print $sindex+1;?>
-		  </td>
-		  <td>
-			<span title="<?php print $comment['body'];?>">
-			<a onclick="parent.viewBook('infobook');" target="viewinfobook"  
-			  href="infobook.php?current=comments_list.php&sid=<?php print $sid;?>"
-			  class="<?php print $comment['class'];?>">C</a> 
-			</span>
-		  </td>
-		  <td>
-			<a href="seneeds.php?current=sen_view.php&sid=<?php print $sid;?>">
-			  <?php print $Student['DisplayFullName']['value']; ?>
-			</a>
-		  </td>
-		  <td>
-			<?php	print $Student['RegistrationGroup']['value']; ?>
-		  </td>
+				<tr>
+					<td>
+						<input type="checkbox" name="sids[]" value="<?php print $sid; ?>" />
+						<?php print $sindex+1;?>
+					</td>
+					<td>
+						<span title="<?php print $comment['body'];?>">
+						  <a onclick="parent.viewBook('infobook');" target="viewinfobook"  
+							href="infobook.php?current=comments_list.php&sid=<?php print $sid;?>"
+							class="<?php print $comment['class'];?>">C</a> 
+						</span>
+					</td>
+					<td>
+						<a href="seneeds.php?current=sen_view.php&sid=<?php print $sid;?>">
+							<?php print $Student['DisplayFullName']['value']; ?>
+						</a>
+					</td>
+					<td>
+						<?php print $Student['RegistrationGroup']['value']; ?>
+					</td>
 <?php
 				foreach($displayfields as $displayfield){
 					if(array_key_exists($displayfield,$Student)){
@@ -156,36 +191,44 @@ if(sizeof($sids)>0){
 						}
 					}
 ?>
-		</tr>
+				</tr>
 <?php
 			}
 		}
 	/* Must unset or the host page thinks a single sid is being displayed*/
 	unset($sid);
 ?>
-
-<tr>
-  <th colspan="<?php print $displayfields_no+3;?>">&nbsp;</th>
-  <th>
-	<div class="rowaction">
+			</tbody>
+			<tfoot class="noprint">
+				<tr>
+					<th colspan="<?php print $displayfields_no+3;?>">&nbsp;</th>
+					<th>
+						<div class="rowaction">
 
 <?php
-$extrabuttons=array();
-$extrabuttons['addcolumn']=array('title'=>'addcolumn','name'=>'extracol','value'=>'yes');
-all_extrabuttons($extrabuttons,'infobook','processContent(this)')
+	$extrabuttons=array();
+	$extrabuttons['addcolumn']=array('title'=>'addcolumn','name'=>'extracol','value'=>'yes');
+	all_extrabuttons($extrabuttons,'infobook','processContent(this)')
 ?>
-	</div>
-  </th>
-</tr>
+						</div>
 
-	</table>
+					</th>
+				</tr>
+			</tfoot>
+		</table>
 
-	<input type="hidden" name="colno" value="<?php print $displayfields_no;?>" />
-	<input type="hidden" name="current" value="<?php print $action;?>" />
-	<input type="hidden" name="cancel" value="<?php print '';?>" />
-	<input type="hidden" name="choice" value="<?php print $choice;?>" />
-  </form>
+		<input type="hidden" name="colno" value="<?php print $displayfields_no;?>" />
+		<input type="hidden" name="current" value="<?php print $action;?>" />
+		<input type="hidden" name="cancel" value="<?php print '';?>" />
+		<input type="hidden" name="choice" value="<?php print $choice;?>" />
+	</form>
 </div>
+
 <?php
 	}
 ?>
+
+<script type="text/javascript">
+	var TSort_Data = new Array ('sidtable','','','',''<?php print $sort_types;?>);
+	tsRegister();
+</script>
