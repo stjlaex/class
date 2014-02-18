3c25940248828d0cd259d5c6e3da2a6b62db52fc
Marius Avram <marius@learningdata.ie>
2014-02-18 10:21:01 +0000

    temporary solution for displaying images only inside ->eportfoliosite

--- scripts/epf_file_display.php
+++ scripts/epf_file_display.php
@@ -7,7 +7,7 @@ require_once('../../school.php');
 require_once('../classdata.php');
 include('../lib/functions.php');
 require_once('../lib/ldap.php');
-
+$filename='';
 if(isset($_GET['fileid'])){$fileid=clean_text($_GET['fileid']);}else{$fileid=-1;}
 if(isset($_GET['location'])){$location=clean_text($_GET['location']);}else{$location='';}
 if(isset($_GET['filename'])){$filename=clean_text($_GET['filename']);}
@@ -21,7 +21,20 @@ if($filename!=''){
 
 $filepath=$CFG->eportfolio_dataroot. '/'.$location;
 
-if($filename!='' and $extension and $_SERVER['HTTP_HOST']==$CFG->eportfoliosite){
+
+if(isset($_SERVER['HTTPS'])){
+	$httpcheck='https';
+	}
+else{
+	$httpcheck='http';
+	}
+
+
+//$requesturl=$httpcheck."://".$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'];
+$requesturl=$_SERVER['HTTP_REFERER'];
+
+if(substr($requesturl,0,strlen($CFG->eportfoliosite))==$CFG->eportfoliosite){$display=true;}
+if($filename!='' and $extension and $display){
 	header('Content-type: ' . $mimetype);
 
 	readfile($filepath);
