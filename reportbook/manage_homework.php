4f5ce1c71e1d94bf7f9a30ec30aa000ea0ef1729
Sidimar <sidimar@learningdata.ie>
2014-03-05 16:54:17 +0000

    Report updated.

--- reportbook/manage_homework.php
+++ reportbook/manage_homework.php
@@ -81,122 +81,112 @@ foreach($stages as $stage){
 		}
 	}
 ?>
-  <div class="content" id="viewcontent">
-
-	<fieldset class="center divgroup">
-	  <legend><?php print_string('homework',$book);?></legend>
-	  <table class="listmenu">
-		<thead>
-		  <tr>
-			<th><?php print_string('teacher');?></th>
-
-<?php
-		foreach($stages as $stage){
-?>
-			<th><?php print $stage['name'];?></th>
-<?php
-			}
-?>
-			<th><?php print_string('total');?></th>
-			<th><?php print_string('average');?></th>
-		  </tr>
-		</thead>
-<?php
-	foreach($noyearhws as $tid => $hws){
-		$tot=0;
-		$cidno=0;
-?>
-		<tr>
-		  <td><?php print get_teachername($tid);?></td>
-<?php
-		foreach($stages as $stage){
-			$stagid=$stage['id'];
-			$stagetot=0;
-			$stagelist='';
-?>
-		  <td>
-<?php
-			if(isset($hws[$stagid])){
-				if(!isset($noweekhws[$tid][$stagid])){$weektot=0;}
-				else{$weektot=$noweekhws[$tid][$stagid];}
-				foreach($hws[$stagid] as $hw){
-					$tot+=$hw['no'];
-					$stagetot+=$hw['no'];
-					$cidno++;
-					$stagelist.= $hw['cid'].' - '.$hw['no'].' <br />';
-					}
-?>
-			<span title="<?php print $stagelist;?>">
-			  <?php print $weektot.'  ( '.$stagetot.' )';?>
-			</span>
-<?php
-				}
-?>
-		  </td>
-<?php
-			}
-?>
-		  <td><?php print $tot;?></td>
-		  <td><?php print round($tot/$cidno);?></td>
-		</tr>
-<?php
-		}
-?>
-	  </table>
-	</fieldset>
-
-
-
-	<fieldset class="center divgroup">
-	  <legend><?php print get_string('subject').' '.get_string('homework');?></legend>
-	  <table class="listmenu">
-		<thead>
-		  <tr>
-			<th style="width:40%;"><?php print_string('subject',$book);?></th>
-<?php
-		foreach($stages as $stage){
-?>
-			<th><?php print $stage['name'];?></th>
-<?php
-			}
-?>
-		  </tr>
-		</thead>
-<?php
-	foreach($subjects as $subject){
-		$bid=$subject['id'];
-		$tot=0;
-?>
-		<tr>
-		  <td><?php print $subject['name'];?></td>
-<?php
-		foreach($stages as $stage){
-			$stagid=$stage['id'];
-			$stagetot=0;
-			$stagelist='';
-?>
-		  <td>
-<?php
-			if(isset($nosubjecthws[$stagid][$bid])){
-				print $nosubjecthws[$stagid][$bid]['week'].'  ( '.$nosubjecthws[$stagid][$bid]['year'].' )';
-				}
-?>
-		  </td>
-<?php
-			}
-?>
-		</tr>
-<?php
-		}
-?>
-	  </table>
-	</fieldset>
-
- 	  <form id="formtoprocess" name="formtoprocess" method="post"
-		action="<?php print $host; ?>" >
-
-	  <input type="hidden" name="current" value="<?php print $action;?>" />
-	  <input type="hidden" name="choice" value="<?php print $choice;?>" />
-	  <input type="hidden" name="cancel" value="<?php print '';?>" />
-	</form>
-  </div>
+    <div class="content" id="viewcontent">
+        <fieldset class="divgroup">
+            <h5><?php print_string('homework',$book);?></h5>
+        <table class="listmenu">
+            <thead>
+                <tr>
+                    <th><?php print_string('teacher');?></th>
+                    <?php
+                        foreach($stages as $stage){
+                    ?>
+                    <th><?php print $stage['name'];?></th>
+                    <?php
+                        }
+                    ?>
+                    <th><?php print_string('total');?></th>
+                    <th><?php print_string('average');?></th>
+                </tr>
+            </thead>
+            <?php
+            foreach($noyearhws as $tid => $hws){
+            	$tot=0;
+            	$cidno=0;
+            ?>
+            <tr>
+                <td><?php print get_teachername($tid);?></td>
+                <?php
+                foreach($stages as $stage){
+                	$stagid=$stage['id'];
+                $stagetot=0;
+                $stagelist='';
+                ?>
+                <td>
+                    <?php
+                    if(isset($hws[$stagid])){
+                    	if(!isset($noweekhws[$tid][$stagid])){$weektot=0;}
+                    	else{$weektot=$noweekhws[$tid][$stagid];}
+                    	foreach($hws[$stagid] as $hw){
+                    		$tot+=$hw['no'];
+                    $stagetot+=$hw['no'];
+                    $cidno++;
+                    $stagelist.= $hw['cid'].' - '.$hw['no'].' <br />';
+                    }
+                    ?>
+                    <span title="<?php print $stagelist;?>"> <?php print $weektot.'  ( '.$stagetot.' )';?></span>
+                    <?php
+                        }
+                    ?>
+                </td>
+                <?php
+                    }
+                ?>
+                <td><?php print $tot;?></td>
+                <td><?php print round($tot/$cidno);?></td>
+            </tr>
+            <?php
+                }
+            ?>
+        </table>
+        </fieldset>
+        <fieldset class="divgroup">
+            <h5><?php print get_string('subject').' '.get_string('homework');?></h5>
+            <table class="listmenu">
+                <thead>
+                    <tr>
+                        <th style="width:40%;"><?php print_string('subject',$book);?></th>
+                        <?php
+                            foreach($stages as $stage){
+                        ?>
+                        <th><?php print $stage['name'];?></th>
+                        <?php
+                            }
+                        ?>
+                    </tr>
+                </thead>
+                <?php
+                foreach($subjects as $subject){
+                    $bid=$subject['id'];
+                $tot=0;
+                ?>
+                <tr>
+                    <td><?php print $subject['name'];?></td>
+                        <?php
+                        foreach($stages as $stage){
+                        	$stagid=$stage['id'];
+                        $stagetot=0;
+                        $stagelist='';
+                        ?>
+                    <td>
+                        <?php
+                        if(isset($nosubjecthws[$stagid][$bid])){
+                        	print $nosubjecthws[$stagid][$bid]['week'].'  ( '.$nosubjecthws[$stagid][$bid]['year'].' )';
+                        }
+                        ?>
+                    </td>
+                    <?php
+                        }
+                    ?>
+                </tr>
+                <?php
+                    }
+                ?>
+            </table>
+        </fieldset>
+        <form id="formtoprocess" name="formtoprocess" method="post" action="<?php print $host; ?>" >
+        <input type="hidden" name="current" value="<?php print $action;?>" />
+        <input type="hidden" name="choice" value="<?php print $choice;?>" />
+        <input type="hidden" name="cancel" value="<?php print '';?>" />
+        </form>
+    </div>
