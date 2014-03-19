b6612ebfbbdb088cc781d1fa70e175973998afd6
Stuart <stuart@learningdata.net>
2014-03-19 11:11:34 +0000

    switch to new design 2.0.0b1

--- classdata.php
+++ classdata.php
@@ -2,7 +2,7 @@
 require_once('include.php');
 $CFG->books=$books;
 $CFG->roles=$roles;
-$CFG->version='1.0.64';
+$CFG->version='2.0.0b1';
 $CFG->dirroot=$CFG->installpath.'/'.$CFG->applicationdirectory;
 global $CFG;
 $session='ClaSS'.$CFG->shortname;
