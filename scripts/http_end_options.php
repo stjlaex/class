5f5fb124a242a60f59dc6242581a2e28ac32bf6d
Marius Avram <marius@learningdata.ie>
2014-04-14 10:31:19 +0100

    html response modified for an iframe working on #121

--- scripts/http_end_options.php
+++ scripts/http_end_options.php
@@ -9,8 +9,23 @@
 		if(!$xmlechoer){
 			$xml=xmlpreparer($rootName,$returnXML);
 			$xml='<'.'?xml version="1.0" encoding="utf-8"?'.'>'.$xml.'';
-			$html="<link rel='stylesheet' type='text/css' href='../templates/".$profile['transform'].".css' media='all' title='Template Output' />"
-				.xmlprocessor($xml,$profile['transform'].'.xsl');
+			$html="<!DOCTYPE html>
+			<head>
+			<meta charset=\"utf-8\">
+			<link rel='stylesheet' type='text/css' href='../templates/".$profile['transform'].".css' media='all' title='Template Output' />
+			<script language='JavaScript' type='text/javascript' src='js/raphael.js' charset='utf-8'></script>
+			<script language='JavaScript' type='text/javascript' src='js/g.raphael-min.js' charset='utf-8'></script>
+			<script language='JavaScript' type='text/javascript' src='js/g.bar-min.js' charset='utf-8'></script>
+			<script language='JavaScript' type='text/javascript' src='js/d3/d3.v3.min.js' charset='utf-8'></script>
+			<script language='JavaScript' type='text/javascript' src='js/jcrop/jquery.min.js' charset='utf-8'></script>
+			<script language='JavaScript' type='text/javascript' src='../templates/".$profile['transform'].".js' charset='utf-8'></script>
+			<meta http-equiv='pragma' content='no-cache'/>
+			<meta http-equiv='Expires' content='0'/>
+			</head>
+			<body onLoad=\"".$profile['transform']."(); alert('Hello');\">"
+			.xmlprocessor($xml,$profile['transform'].'.xsl')
+			."</body>;
+			</html>";
 			header('Content-Type: text/html'); 
 			$array=array("html"=>$html,"template"=>$profile['transform']);
 			echo json_encode($array);
