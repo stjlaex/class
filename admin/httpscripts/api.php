457fb699ad11a1dc53be048800f22d96abfbfb60
Marius Avram <marius@learningdata.ie>
2014-05-09 16:37:19 +0100

    fix for uploading images working on #162

--- admin/httpscripts/api.php
+++ admin/httpscripts/api.php
@@ -14,29 +14,39 @@
 	$username=$_GET['username'];
 	$token=$_GET['password'];
 
-	$postdata=1;
-	if($_GET['post']=='true'){
-		$script='api_postqueries.php';
-		$postdata=json_decode(file_get_contents('php://input'),true);
+	if($username!='' and $token!='' and $action!=''){
+		$postdata=1;
+		if($_GET['post']=='true'){
+			$script='api_postqueries.php';
+			$content=file_get_contents('php://input');
+			$postdata=array('data'=>$content);
+			}
+		else{$script='api_getqueries.php';}
+
+		if(isset($_SERVER['HTTPS'])){$http='https';}
+		else{$http='http';}
+		$url=$http.'://'.$CFG->siteaddress.$CFG->sitepath.'/'.$CFG->theme20.'/admin/httpscripts/'.$script.'?username=' 
+				.$username.'&password='.$token.'&action='.$action;
+
+		$curl=curl_init();
+		curl_setopt($curl,CURLOPT_SSL_VERIFYPEER,0);
+		curl_setopt($curl,CURLOPT_URL,$url);
+		curl_setopt($curl,CURLOPT_POST,1);
+		curl_setopt($curl,CURLOPT_RETURNTRANSFER,1);
+		curl_setopt($curl,CURLOPT_POSTFIELDS,$postdata);
+		$response=curl_exec($curl);
+		curl_close($curl);
 		}
-	else{$script='api_getqueries.php';}
-
-	if(isset($_SERVER['HTTPS'])){$http='https';}
-	else{$http='http';}
-	$url=$http.'://'.$CFG->siteaddress.$CFG->sitepath.'/'.$CFG->theme20.'/admin/httpscripts/'.$script.'?username=' 
-			.$username.'&password='.$token.'&action='.$action;
-
-	$curl=curl_init();
-	curl_setopt($curl,CURLOPT_SSL_VERIFYPEER,0);
-	curl_setopt($curl,CURLOPT_URL,$url);
-	curl_setopt($curl,CURLOPT_POST,1);
-	curl_setopt($curl,CURLOPT_RETURNTRANSFER,1);
-	curl_setopt($curl,CURLOPT_POSTFIELDS, $postdata);
-	$response=curl_exec($curl);
-	curl_close($curl);
+	else{
+		$response['success']='false';
+		$response['errors'][]='Invalid request!';
+		$response['errors'][]='Authentication and action needed.';
+		$response['errors'][]='Go to http://wiki.classlearning.net/doku.php?id=api for documentation.';
+		$response=json_encode($response);
+		}
 
 	header("Access-Control-Allow-Origin: *");
 	header("Access-Control-Allow-Headers: Content-Type");
-	header('Content-Type: application/json'); 
+	header("Content-Type: application/json; charset=utf-8"); 
 	echo $response;
 ?>
--- admin/httpscripts/api_postqueries.php
+++ admin/httpscripts/api_postqueries.php
@@ -3,43 +3,48 @@
 require('../../scripts/api_head_options.php');
 
 $action=$_GET['action'];
+$postdata=(array)json_decode(stripslashes($_POST['data']),true);
 
 if($action=='poststatementphoto'){
-	$students=$_POSTls['students'];
-	$statement=$_POST['statement'];
-	$photos=$_POST['photos'];
-	//if(!is_array($photos)){$photos[]=$photos;}
-	//if(!is_array($students)){$students[]=$students;}
+	$students=$postdata['students'];
+	$statement=$postdata['statement'];
+	$photos=$postdata['photos'];
+
+	if(count($students)==0 or count($photos)==0 or count($statement)==0){$errors[]='Invalid parameters';}
 
 	$result['success']='true';
 	$result['action']=$action;
-	//$result['test']=$_POST;
 
 	foreach($photos as $photo){
-		$image=$photo;
-		if(strpos($image,'image/jpeg')!==false){$filetype='image/jpeg';$extension='.jpg';}
-		elseif(strpos($image,'image/png')!==false){$filetype='image/png';;$extension='.png';}
-		elseif(strpos($image,'image/gif')!==false){$filetype='image/gif';;$extension='.gif';}
-		$image=str_replace('data:'.$filetype.';base64,', '', $image);
+		$image=$photo['photo'];
+		list($type, $image)=explode(';', $image);
+		list($type, $filetype)=explode(':', $type);
+		list($encode, $image)=explode(',', $image);
+		if($filetype==='image/jpeg'){$extension='.jpg';}
+		elseif($filetype==='image/png'){$extension='.png';}
+		elseif($filetype==='image/gif'){$extension='.gif';}
 		$image=str_replace(' ', '+', $image);
 		$data=base64_decode($image);
+		$filename=$photo['name'].$extension;
+		$uniquename=uniqid().$extension;
 
 		foreach($students as $sid){
 			$d_e=mysql_query("SELECT epfusername FROM info WHERE student_id='$sid';");
 			$epfu=mysql_result($d_e,0,'epfusername');
-			$dir=$CFG->eportfolio_dataroot.'/files/'.substr($epfu,0,1).'/'.$epfu.'/';
-			$dir='/home/epfdata/cache/files';
-			$file=$dir.'/apitestfile'.uniqid().$extension;
-			$success=file_put_contents($file, $data);
+			$dir=$CFG->eportfolio_dataroot.'/files/'.substr($epfu,0,1).'/'.$epfu.'';
+			$file=$dir.'/'.$uniquename;
+			if(file_put_contents($file, $data)){$success='true';}
+			else{$success='false';}
 			$result['photos'][]=array(
 				'success'=>$success,
 				'sid'=>$sid,
-				'filepath'=>$file
+				'filepath'=>$file,
+				'filename'=>$filename
 				);
 
-			$publishdata['foldertype']=$context;
+			$publishdata['foldertype']='assessment';
 			$publishdata['title']='';
-			$publishdata['batchfiles'][]=array('epfusername'=>$owner,
+			$publishdata['batchfiles'][]=array('epfusername'=>$epfu,
 											   'filename'=>$uniquename,
 											   'originalname'=>$filename,
 											   'linkedid'=>$linkedid,
@@ -51,7 +56,7 @@ if($action=='poststatementphoto'){
 		}
 	}
 else{
-	$errors[]=print_string('invalidaction','admin').": $action";
+	$errors[]="Invalid action: $action";
 	}
 
 require('../../scripts/api_end_options.php');
