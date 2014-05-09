d79d9107d3ae46bf46d56740932fa2c466047bd0
Marius Avram <marius@learningdata.ie>
2014-05-09 10:23:12 +0100

    added api for classis working on #162

---
+++ admin/httpscripts/api_curl.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ *											classis_api.php
+ *
+ *	@package		Classis
+ *	@author		stj@laex.org
+ *	@copyright	S T Johnson 2004-2014
+ *	@version
+ *	@since
+ */
+ require_once('../../../school.php');
+
+	$action=$_GET['action'];
+	$username=$_GET['username'];
+	$token=$_GET['password'];
+
+	$postdata=1;
+	if($_GET['post']=='true'){
+		$script='api_postqueries.php';
+		$postdata=json_decode(file_get_contents('php://input'),true);
+		}
+	else{$script='api_getqueries.php';}
+
+	if(isset($_SERVER['HTTPS'])){$http='https';}
+	else{$http='http';}
+	$url=$http.'://'.$CFG->siteaddress.$CFG->sitepath.'/'.$CFG->applicationdirectory.'/admin/httpscripts/'.$script.'?username=' 
+			.$username.'&password='.$token.'&action='.$action;
+
+	$curl=curl_init();
+	curl_setopt($curl,CURLOPT_SSL_VERIFYPEER,0);
+	curl_setopt($curl,CURLOPT_URL,$url);
+	curl_setopt($curl,CURLOPT_POST,1);
+	curl_setopt($curl,CURLOPT_RETURNTRANSFER,1);
+	curl_setopt($curl,CURLOPT_POSTFIELDS, $postdata);
+	$response=curl_exec($curl);
+	curl_close($curl);
+
+	echo $response;
+?>
