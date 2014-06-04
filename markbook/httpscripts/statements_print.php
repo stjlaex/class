1cb3b6c960461806d4ea60465d4b77e934412b70
Marius Avram <marius@learningdata.ie>
2014-06-04 10:08:37 +0100

    added button for statements list print

---
+++ markbook/httpscripts/statements_print.php
@@ -0,0 +1,28 @@
+<?php
+/**									statements_print.php
+ *
+ */
+
+require_once('../../scripts/http_head_options.php');
+
+if(isset($_GET['rids']) and $_GET['rids']!=''){$rids=$_GET['rids'];}
+if(isset($_GET['stage']) and $_GET['stage']!=''){$stage=$_GET['stage'];}
+
+foreach($rids as $rid){
+	$stagestatements=get_report_skill_statements($rid,'%','%',$stage);
+	foreach($stagestatements as $stagestatement){
+		$Statements['Statement'][]=$stagestatement;
+		}
+	$Statements['rids'][]=$rid;
+	}
+
+$Statements['Stage']=$stage;
+$Statements['Paper']='portrait';
+$Statements['Transform']='statements_print';
+
+$returnXML=$Statements;
+$rootName='Statements';
+
+require_once('../../scripts/http_end_options.php');
+exit;
+?>
