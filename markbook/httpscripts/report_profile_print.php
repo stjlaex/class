a46f1a31e8836bcb4a1d3a159ea0aef32bee6ffa
Marius Avram <marius@learningdata.ie>
2014-07-01 08:48:47 +0100

    added transform tracking_chart_pips

--- markbook/httpscripts/report_profile_print.php
+++ markbook/httpscripts/report_profile_print.php
@@ -160,6 +160,12 @@ else{
 		//$prev_AssDefs=(array)fetch_cohortAssessmentDefinitions($prevcohort,$profile['id']);
 		//$AssDefs=(array)array_merge($AssDefs,$prev_AssDefs);
 		}
+	elseif($profile['transform']=='tracking_chart_pips'){
+		$bid='G';$pid='';
+		$prevcohort=array('id'=>'','course_id'=>$crid,'stage'=>'%','year'=>'%');
+		//$prev_AssDefs=(array)fetch_cohortAssessmentDefinitions($prevcohort,$profile['id']);
+		//$AssDefs=(array)array_merge($AssDefs,$prev_AssDefs);
+		}
 	elseif($profile['transform']=='tracking_sheet'){
 		$pid='%';$bid='%';
 		$AssDefs=(array)fetch_cohortAssessmentDefinitions($cohort,$profile['id']);
