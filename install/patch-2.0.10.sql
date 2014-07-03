de096e5df93af2f5cad53fdb2f24321fa8e11e22
Marius Avram <marius@learningdata.ie>
2014-07-03 14:55:15 +0100

    added transfer type community to type field

--- install/patch-2.0.10.sql
+++ install/patch-2.0.10.sql
@@ -1,2 +1,2 @@
-ALTER TABLE community CHANGE type type ENUM('','academic','family','form','year','tutor','alumni','enquired','applied','accepted','trip','reg','transport','extra','house','accomodation','new') NOT NULL DEFAULT '';
+ALTER TABLE community CHANGE type type ENUM('','academic','family','form','year','tutor','alumni','enquired','applied','accepted','trip','reg','transport','extra','house','accomodation','new','transfer') NOT NULL DEFAULT '';
 ALTER TABLE report  ADD type ENUM('wrapper','subject','profile') NOT NULL DEFAULT 'wrapper';
