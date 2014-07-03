3ba67c83c4dfeb4be20aa49ad7427e8175883276
Marius Avram <marius@learningdata.ie>
2014-07-03 10:50:37 +0100

    added type field for report table

--- install/patch-2.0.10.sql
+++ install/patch-2.0.10.sql
@@ -1,1 +1,2 @@
-ALTER TABLE community CHANGE type type ENUM('','academic','family','form','year','tutor','alumni','enquired','applied','accepted','trip','reg','transport','extra','house','accomodation','new') NOT NULL DEFAULT ''
+ALTER TABLE community CHANGE type type ENUM('','academic','family','form','year','tutor','alumni','enquired','applied','accepted','trip','reg','transport','extra','house','accomodation','new') NOT NULL DEFAULT '';
+ALTER TABLE report  ADD type ENUM('wrapper','subject','profile') NOT NULL DEFAULT 'wrapper';
