80ecb12732d280fc3b774a453bf4a2e03369496c
Marius Avram <marius@learningdata.ie>
2014-08-08 13:00:39 +0100

    new iteration

--- classdata.php
+++ classdata.php
@@ -2,7 +2,7 @@
 require_once('include.php');
 $CFG->books=$books;
 $CFG->roles=$roles;
-$CFG->version='2.0.12';
+$CFG->version='2.0.13';
 $CFG->dirroot=$CFG->installpath.'/'.$CFG->applicationdirectory;
 global $CFG;
 $session='ClaSS'.$CFG->shortname;
