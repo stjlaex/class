bfba9617b3cd7a5925e876ff8d0d83a604d5d5ea
Marius Avram <marius@learningdata.ie>
2014-09-18 11:03:33 +0100

    payee and iban added to invoice export close #267

--- admin/fees_invoice_export.php
+++ admin/fees_invoice_export.php
@@ -14,7 +14,20 @@ if((isset($_GET['remid']) and $_GET['remid']!='')){$remid=$_GET['remid'];}
 if((isset($_POST['paymenttype']) and $_POST['paymenttype']!='')){$paymenttype=$_POST['paymenttype'];}else{$paymenttype='';}
 if((isset($_GET['paymenttype']) and $_GET['paymenttype']!='')){$paymenttype=$_GET['paymenttype'];}
 
-
+function checksumDigits($string,$country="ES"){
+	$letters=array("A"=>"10","B"=>"11","C"=>"12","D"=>"13","E"=>"14","F"=>"15",
+				"G"=>"16","H"=>"17","I"=>"18","J"=>"19","K"=>"20","L"=>"21",
+				"M"=>"22","N"=>"23","O"=>"24","P"=>"25","Q"=>"26","R"=>"27",
+				"S"=>"28","T"=>"29","U"=>"30","V"=>"31","W"=>"32","X"=>"33",
+				"Y"=>"34","Z"=>"35"
+				);
+	$code=$string.$country."00";
+	$ncode=strtr($code,$letters);
+	$rest=bcmod($ncode,97);
+	$digits=98-$rest;
+	if($digits<10){$digits=str_pad($digits,2,"0",STR_PAD_LEFT);}
+	return $digits;
+	}
 
 $Students=array();
 $Remittance=fetchRemittance($remid);
@@ -57,15 +70,19 @@ $invoices=(array)list_remittance_invoices($remid,$paymenttype);
 
 		$worksheet->write(1, 0, '', $format);
 		$worksheet->write(1, 1, '', $format);
-		$worksheet->write(1, 2, '', $format_head);
-		$worksheet->write(1, 3, get_string('remittance',$book), $format_head);
-		$worksheet->write(1, 4, get_string('date',$book), $format_head);
-		$worksheet->write(1, 5, get_string('total',$book), $format_head);
+		$worksheet->write(1, 2, '', $format);
+		$worksheet->write(1, 3, '', $format);
+		$worksheet->write(1, 4, '', $format_head);
+		$worksheet->write(1, 5, get_string('remittance',$book), $format_head);
+		$worksheet->write(1, 6, get_string('date',$book), $format_head);
+		$worksheet->write(1, 7, get_string('total',$book), $format_head);
 		$worksheet->write(2, 0, '', $format);
 		$worksheet->write(2, 1, '', $format);
 		$worksheet->write(2, 2, '', $format);
-		$worksheet->write(2, 3, $Remittance['Name']['value'], $format);
-		$worksheet->write(2, 4, display_date($Remittance['IssueDate']['value']), $format);
+		$worksheet->write(2, 3, '', $format);
+		$worksheet->write(2, 4, '', $format);
+		$worksheet->write(2, 5, $Remittance['Name']['value'], $format);
+		$worksheet->write(2, 6, display_date($Remittance['IssueDate']['value']), $format);
 
 		$worksheet->write(4, 0, '', $format_head);
 		$worksheet->write(4, 1, get_string('invoice',$book), $format_head);
@@ -73,6 +90,8 @@ $invoices=(array)list_remittance_invoices($remid,$paymenttype);
 		$worksheet->write(4, 3, get_string('formgroup',$book), $format_head);
 		$worksheet->write(4, 4, get_string('payment',$book), $format_head);
 		$worksheet->write(4, 5, get_string('amount',$book), $format_head);
+		$worksheet->write(4, 6, get_string('payee',$book), $format_head);
+		$worksheet->write(4, 7, get_string('account',$book), $format_head);
 
 
 		$total=0;
@@ -81,7 +100,15 @@ $invoices=(array)list_remittance_invoices($remid,$paymenttype);
 			$rowno++;
 			$Invoice=(array)fetchFeesInvoice($invoice);
 			$sid=$Invoice['student_id_db'];
-			
+			$accountid=$Invoice['account_id_db'];
+			$Account=(array)fetchAccount($accountid,'id');
+			if($Account['Iban']['value']!='' and checkIBAN($Account['Iban']['value'])){$IBAN=$Account['Iban']['value'];}
+			else{
+				$accountno=$Account['BankCode']['value'].$Account['Branch']['value'].$Account['Control']['value'].$Account['Number']['value'];
+				$ibandigits=checksumDigits($accountno);
+				$IBAN="ES".$ibandigits.$accountno;
+				}
+
 			if(!array_key_exists($sid,$Students)){
 				/* Do this once only for a student... */
 				$Student=(array)fetchStudent_short($sid);
@@ -97,11 +124,13 @@ $invoices=(array)list_remittance_invoices($remid,$paymenttype);
 			$worksheet->write($rowno, 3, iconv('UTF-8','ISO-8859-1',$Student['RegistrationGroup']['value']), $format);
 			$worksheet->write($rowno, 4, iconv('UTF-8','ISO-8859-1',get_string(displayEnum($Invoice['PaymentType']['value'],'paymenttype'),$book)), $format);
 			$worksheet->write($rowno, 5, iconv('UTF-8','ISO-8859-1',display_money($Invoice['TotalAmount']['value'])), $format);
+			$worksheet->write($rowno, 6, iconv('UTF-8','ISO-8859-1',$Invoice['AccountName']['value']), $format);
+			$worksheet->write($rowno, 7, iconv('UTF-8','ISO-8859-1',$IBAN), $format);
 			$total+=$Invoice['TotalAmount']['value'];
 			}
 
 		/* The final Total */
-		$worksheet->write(2, 5, display_money($total), $format);
+		$worksheet->write(2, 7, display_money($total), $format);
 
 		/*send the workbook w/ spreadsheet and close them*/ 
 		$workbook->close();
