8cf0c3b2498a845ef613dd0ff5517f245b5b590c
Marius Avram <marius@learningdata.ie>
2014-09-22 10:00:24 +0100

    register week print template shows the periods

--- register/httpscripts/register_week_print.php
+++ register/httpscripts/register_week_print.php
@@ -40,9 +40,13 @@ else{
 			$students=(array)listin_community($com);
 			foreach($students as $student){
 				$Student=fetchStudent_short($student['id']);
+				$sectionid=get_student_section($student['id']);
 				for($i=0;$i<7;$i++){
 					$weekday=date( "Y-m-d", strtotime('-'.$i.' days'));
-					$sessions=array('AM','PM');
+					if(isset($CFG->registration[$sectionid])){$secid=$sectionid;}
+					else{$secid=1;}
+					if($CFG->registration[$secid]=='single'){$sessions=array('AM');}
+					else{$sessions=array('AM','PM');}
 					foreach($sessions as $session){
 						$e=get_event($weekday,$session);
 						if($e['id']!='' and date('l',strtotime($weekday))!='Saturday' and date('l',strtotime($weekday))!='Sunday'){
@@ -54,12 +58,32 @@ else{
 							$Student['Attendances']['Attendance'][]['Date']['value']=$weekday;
 							}
 						}
+
+						if(isset($CFG->regperiods)){
+							if(isset($CFG->regperiods[$sectionid])){$secid=$sectionid;}
+							else{$secid=1;}
+							if(isset($CFG->regperiods[$secid][$session])){
+								foreach($CFG->regperiods[$secid][$session] as $periodid=>$period){
+									$Students['Periods'][]=array('section'=>$secid,'session'=>$session,'title'=>$periodid);
+									$e=get_event($weekday,$session,$periodid);
+									if($e['id']!='' and date('l',strtotime($weekday))!='Saturday' and date('l',strtotime($weekday))!='Sunday'){
+										$att=fetchcurrentAttendance($student['id'],$e['id']);
+										if($att['Date']['value']==''){$att['Date']['value']=$weekday;$att['Session']['value']=$session;$att['Period']['value']=$periodid;}
+										$Student['Attendances']['Attendance'][]=$att;
+										}
+									if($e['id']=='' and date('l',strtotime($weekday))!='Saturday' and date('l',strtotime($weekday))!='Sunday'){
+										$Student['Attendances']['Attendance'][]['Date']['value']=$weekday;
+										$Student['Attendances']['Attendance'][]['Period']['value']=$periodid;
+										}
+									}
+								}
+							}
 						$week[$weekday]=$weekday;
 					}
 					$Community['Student'][]=$Student;
 				}
 				foreach($week as $day){
-					if(date('l',strtotime($day))!='Saturday' and date('l',strtotime($day))!='Sunday'){$Students['Dates'][]=array('value'=>display_date($day),'day'=>date('l',strtotime($day)));}
+					if(date('l',strtotime($day))!='Saturday' and date('l',strtotime($day))!='Sunday'){$Students['Dates'][]=array('display'=>display_date($day),'value'=>date('Y-m-d',strtotime($day)),'day'=>date('l',strtotime($day)));}
 					}
 				$Students['Community'][]=$Community;
 			}
