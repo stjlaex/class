d9c2d070b6aed951b03cd6220ed68d15d3609708
Marius Avram <marius@learningdata.ie>
2014-09-22 09:46:35 +0100

    added api post action for adding enquiries closes #242

--- admin/httpscripts/api_postqueries.php
+++ admin/httpscripts/api_postqueries.php
@@ -103,7 +103,130 @@ if($action=='poststatementphoto'){
 			}
 		}
 	}
+elseif($action=='postenquiryform'){
+	$formdata=parse_enquiry($postdata);
+	$Contacts=$formdata['Contacts'];
+	foreach($Contacts as $Contact){
+		$email=$Contact['email'];
+		$surname=$Contact['surname'];
+		$forename=$Contact['forename'];
+		$d_g=mysql_query("SELECT id FROM guardian WHERE email='$email';");
+		if(mysql_num_rows($d_g)>0){
+			$gid=mysql_result($d_g,0);
+			}
+		else{
+			mysql_query("INSERT INTO guardian (surname, forename, email) 
+								VALUES ('$surname', '$forename', '$email');");
+			$gid=mysql_insert_id();
+			foreach($Contact['phones'] as $phone){
+				$phoneno=$phone['number'];
+				mysql_query("INSERT INTO phone (some_id, number) 
+									VALUES ('$gid', '$phoneno');");
+				}
+			}
+		$gids[]=$gid;
+		}
+	if(count($gids)>0){
+		$Students=$formdata['Students'];
+		foreach($Students as $Student){
+			$dob=$Student['dob'];
+			$surname=$Student['surname'];
+			$forename=$Student['forename'];
+			$note=$Student['message'];
+			mysql_query("INSERT INTO student (surname, forename, dob) 
+							VALUES ('$surname', '$forename', '$dob');");
+			$sid=mysql_insert_id();
+			mysql_query("INSERT INTO info SET student_id='$sid', appnotes='$note';");
+			foreach($gids as $gid){
+				mysql_query("INSERT INTO gidsid SET guardian_id='$gid', student_id='$sid';");
+				}
+			$enrolstatus='EN';
+			$comtype='enquired';
+			$comname=$enrolstatus.':'.$Student['yeargroup_id'];
+			$community=array('id'=>'','type'=>$comtype,'name'=>$comname,'year'=>$Student['enrolyear']);
+			join_community($sid,$community);
+			$sids[]=$sid;
+			}
+		if(count($sids)>0){
+			$result['success']=true;
+			$result['enquiry']=array(
+				'sids'=>$sids,
+				'gids'=>$gids,
+				'results'=>print_r($postdata,true)
+				);
+			}
+		else{$errors[]="Couldn't add students to ClassIS.";}
+		}
+	else{$errors[]="Couldn't add contacts to ClassIS.";}
+
+	function parse_enquiry($formdata){
+		$Students=array();$Contacts=array();$Data=array();
+		foreach($formdata['students'] as $sno=>$student){
+			foreach($student as $field=>$inval){
+				$Students[$sno][$field]=$inval;
+				if($field=='dob' or $field=='dateofbirth'){
+					$additional_details=get_yeargroup_by_dob($inval);
+					foreach($additional_details as $key=>$detail){
+						$Students[$sno][$key]=$detail;
+						}
+					}
+				}
+			}
+		foreach($formdata['contacts'] as $cno=>$contact){
+			foreach($contact as $field=>$inval){
+				if($field=='dob' or $field=='dateofbirth'){
+					$inval=get_date_ymd($inval);
+					}
+				$Contacts[$cno][$field]=$inval;
+				}
+			}
+		$Data['Students']=$Students;
+		$Data['Contacts']=$Contacts;
+		return $Data;
+		}
+
+	function get_date_ymd($dob){
+		$dob_bits=explode('/',$dob);
+		$dob=$dob_bits[2] .'-'.$dob_bits[1].'-'.$dob_bits[0];
+		return $dob;
+		}
+
+	function get_yeargroup_by_dob($dob){
+		$student=array();
+		$student['dob']=get_date_ymd($dob);
+		$dob_bits=explode('-',$student['dob']);
+
+		$enrolyear=get_curriculumyear()+1;
+		$lookup_index=$enrolyear - $dob_bits[0] - 1;
+		while($lookup_index<2){
+			$enrolyear++;
+			$lookup_index=$enrolyear - $dob_bits[0] - 1;
+			}
+		$yid_lookups=array('2'=>'-2'
+						,'3'=>'-1'
+						,'4'=>'0'
+						,'5'=>'1'
+						,'6'=>'2'
+						,'7'=>'3'
+						,'8'=>'4'
+						,'9'=>'5'
+						,'10'=>'6'
+						,'11'=>'7'
+						,'12'=>'8'
+						,'13'=>'9'
+						,'14'=>'10'
+						,'15'=>'11'
+						,'16'=>'12'
+						,'17'=>'13'
+						);
+		$student['yeargroup_id']=$yid_lookups[$lookup_index];
+		$student['enrolyear']=$enrolyear;
+		return $student;
+		}
+	}
 elseif($action=='postinfobookcomment'){
+	$result['success']=false;
+	$result['action']=$action;
 	/*
 	$sid=170;
 	$postdata['students'][$sid]['sid']=$sid;
