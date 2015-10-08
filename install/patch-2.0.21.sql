853ae225c00d5597b28ccfbccdbc98aea5992390
Marius Avram <marius@learningdata.ie>
2015-10-08 13:59:58 +0100

    created table for subject comments length

--- install/patch-2.0.21.sql
+++ install/patch-2.0.21.sql
@@ -4,3 +4,12 @@ ALTER TABLE comidsid ADD index indexmember (community_id, student_id);
 
 ALTER TABLE mark ADD elgg_weblog_post_id INT;
 ALTER TABLE fees_account ADD code VARBINARY(40) NOT NULL AFTER bic;
+
+CREATE TABLE report_comments_length (
+	id              int unsigned NOT NULL auto_increment,
+        report_id   	int unsigned NOT NULL,
+	subject_id	varchar(10) NOT NULL,
+	comment_length	int unsigned NOT NULL,
+        index           index_result(report_id),
+        primary key     (id)
+);
