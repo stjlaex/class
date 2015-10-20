fb5ef7456baee31d406f85968f7b655659c22636
Stuart Johnson <stj@laex.org>
2015-10-20 10:01:29 +0100

    update the aboutbook info page to remove old ClaSS references and add support centre

--- lang/es/aboutbook.php
+++ lang/es/aboutbook.php
@@ -4,16 +4,15 @@ $string=array(
      "requestfeature"=>"Pedir una característica",
      "contactsupport"=>"Contacte con el soporte",
      "supportprocedure"=>"Procedimiento del soporte",
-     "explainfeaturehelps"=>"Por favor explique que debería permitirte la característica hacer y por que seria útil. Donde debería ubicarse mas o menos en ClaSS?",
-     "submittingbughelps"=>"Presentando un reporte de bug usted ayuda en la rapidez de desarrollo de ClaSS.",
+     "explainfeaturehelps"=>"Por favor explique que debería permitirte la característica hacer y por que seria útil. Donde debería ubicarse mas o menos en Classis?",
+     "submittingbughelps"=>"Presentando un reporte de bug usted ayuda en la rapidez de desarrollo de Classis.",
      "supportexplanation"=>"Este formulario va a enviar un correo 
-a tu contacto local de ClaSS.»<br /> No espere respuesta por correo. Pero usted⏎ sera contactado si se necesitan mas detalles para resolver la petición.",
+a tu contacto local de Classis.»<br /> No espere respuesta por correo. Pero usted⏎ sera contactado si se necesitan mas detalles para resolver la petición.",
      "timetakenappreciated"=>"El tiempo tomado para describir el problema en detalle es muy apreciado.",
-     "classblurb"=>"ClaSS (el Sistema de Estudiantes ClaSS) es un sistema de gestión de seguimiento, reportes e información. ClaSS pretende ser el asistente ubicuo de información de clases para escuelas.",
-     "formoreinformation"=>"Para mas informacion sobre como usar ClaSS, como puede contribuir para mejorar ClaSS y descubrir como ClaSS puede hacer la diferencia para el aprendizaje en tu escuela, echa un vistazo a la pagina del proyecto ClaSS.",
-     "gplintro"=>"Este software tiene copyright (c) 2002 - 2011 S. T. Johnson. Todos los derechos reservados. Este programa es un software libre; puedes redistribuirlo y/o modificarlo bajo las condiciones de la Licencia Publica General GNU Affero versión 3 o (según tu) cualquier versión posterior.",
+     "classblurb"=>"Classis (el Sistema de Estudiantes ClaSS) es un sistema de gestión de seguimiento, reportes e información. Classis pretende ser el asistente ubicuo de información de clases para escuelas.",
+     "formoreinformation"=>"Para mas informacion sobre como usar Classis, como puede contribuir para mejorar Classis y descubrir como Classis puede hacer la diferencia para el aprendizaje en tu escuela, echa un vistazo a la pagina del proyecto Classis.",
+     "gplintro"=>"Este software tiene copyright (c) 2002 - 2015 S. T. Johnson. Todos los derechos reservados. Este programa es un software libre; puedes redistribuirlo y/o modificarlo bajo las condiciones de la Licencia Publica General GNU Affero versión 3 o (según tu) cualquier versión posterior.",
      "documentation"=>"Documentación",
      "usermanual"=>"Manual de usuario",
-     "homepage"=>"Proyecto ClaSS",
-     "classis"=>"ClaSS Information Services"
+     "homepage"=>"Proyecto Classis"
 );?>
