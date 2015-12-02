fd4ebd6ca5d3021f386ff8003c73476c7764f78c
Marius Avram <marius@learningdata.ie>
2015-12-02 14:44:25 +0000

    change classdata version

--- classdata.php
+++ classdata.php
@@ -2,7 +2,7 @@
 require_once('include.php');
 $CFG->books=$books;
 $CFG->roles=$roles;
-$CFG->version='2.0.23';
+$CFG->version='2.0.24';
 $CFG->dirroot=$CFG->installpath.'/'.$CFG->applicationdirectory;
 global $CFG;
 $session='ClaSS'.$CFG->shortname;
