6801984e8c4186be3415785635c263dc21c4fcb2
Marius Avram <marius@learningdata.ie>
2016-02-18 09:25:55 +0000

    fixed stage for clickToConfigureCategories

--- reportbook/httpscripts/category_editor.php
+++ reportbook/httpscripts/category_editor.php
@@ -27,7 +27,9 @@ if($rid!=''){
 	$stages[]=array('id'=>'%','name'=>get_string('allstages','reportbook'));
 	$extrastages=(array)list_course_stages($report['course_id']);
 	$stages=array_merge($stages,$extrastages);
-	//$stage=$report['stage'];//Make the default
+        if($stage==''){
+	    $stage=$report['stage'];//Make the default
+        }
 
 	/*Sublevels*/
 	$isublevel=1;
