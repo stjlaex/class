3208803942b463ac691faa245d05c0cb1d28b3d6
Marius Avram <marius@learningdata.ie>
2016-03-10 11:41:17 +0000

    removed elgg_list_files from student subject reports

--- infobook/student_reports.php
+++ infobook/student_reports.php
@@ -1,5 +1,5 @@
 <?php
-/**                                  student_reports.php
+/**				     student_reports.php
  *
  */
 
@@ -18,58 +18,52 @@ two_buttonmenu($extrabuttons);
 
 $epfu=strtolower($Student['EPFUsername']['value']);
 if($epfu!=''){
-	if($CFG->eportfolio_db==''){
-		/* If epfdb='' then simply list epfuser's directory looking for reports. */
-		$directory='files/' . substr($epfu,0,1) . '/' . $epfu;
-		$report_files=(array)list_directory_files($CFG->eportfolio_dataroot.'/'.$directory,'pdf');
-		}
-	else{
-		$report_files=(array)elgg_list_files($epfu,'report',true);
-		}
-	}
+    $directory='files/' . substr($epfu,0,1) . '/' . $epfu;
+    $report_files=(array)list_directory_files($CFG->eportfolio_dataroot.'/'.$directory,'pdf');
+    }
 ?>
     <div id="heading">
-        <h4><label><?php print_string('subjectreports'); ?></label> <?php print $Student['DisplayFullName']['value'];?></h4>
+	<h4><label><?php print_string('subjectreports'); ?></label> <?php print $Student['DisplayFullName']['value'];?></h4>
     </div>
     <div class="content">
-        <fieldset class="divgroup">
-            <h5><?php print get_string('published','reportbook'). ' '.get_string('reports',$book);?></h5>
-            <?php
-            	if(isset($_SERVER['HTTPS'])){
-            		$http='https';
-            		}
-            	else{
-            		$http='http';
-            		}
-            
-            	if(trim($epfu)==''){$epfu=strtolower($Student['EnrolNumber']['value']);}
-            	foreach($report_files as $reportdetails){
-            		if(!is_array($reportdetails)){$report=array('title'=>$reportdetails,'name'=>$reportdetails.'.pdf','location'=>$directory.'/'.$reportdetails.'.pdf');}
-            		else{$report=$reportdetails;}
-            
-            		/* A mix of PDFs will be uploaded to the epfdata directory for
-            		 * files and we only want to display report PDFs. 
-            		 */
-            		if(strpos($report['name'],'eport')>0 and $report['title']!=''){
-            			print '<div style="float:left;width:24%;margin:2px;padding:2px 4px;background-color:#ffffff;">';
-            			print '<a href="'.$http.'://'.$CFG->siteaddress.$CFG->sitepath.'/'.$CFG->applicationdirectory.'/scripts/file_display.php?epfu='.$epfu.'&location='.$report['location'].'&filename='.$report['name'].'" /><label>'.$report['title'].'</label><img src="images/printer.png" /></a>';
-            			print '</div>';
-            		}
-            	}
-            ?>
-    	</fieldset>
+	<fieldset class="divgroup">
+	    <h5><?php print get_string('published','reportbook'). ' '.get_string('reports',$book);?></h5>
+	    <?php
+		if(isset($_SERVER['HTTPS'])){
+			$http='https';
+			}
+		else{
+			$http='http';
+			}
+	    
+		if(trim($epfu)==''){$epfu=strtolower($Student['EnrolNumber']['value']);}
+		foreach($report_files as $reportdetails){
+			if(!is_array($reportdetails)){$report=array('title'=>$reportdetails,'name'=>$reportdetails.'.pdf','location'=>$directory.'/'.$reportdetails.'.pdf');}
+			else{$report=$reportdetails;}
+	    
+			/* A mix of PDFs will be uploaded to the epfdata directory for
+			 * files and we only want to display report PDFs. 
+			 */
+			if(strpos($report['name'],'eport')>0 and $report['title']!=''){
+				print '<div style="float:left;width:24%;margin:2px;padding:2px 4px;background-color:#ffffff;">';
+				print '<a href="'.$http.'://'.$CFG->siteaddress.$CFG->sitepath.'/'.$CFG->applicationdirectory.'/scripts/file_display.php?epfu='.$epfu.'&location='.$report['location'].'&filename='.$report['name'].'" /><label>'.$report['title'].'</label><img src="images/printer.png" /></a>';
+				print '</div>';
+			}
+		}
+	    ?>
+	</fieldset>
 	<fieldset class="divgroup">
 
-        <form id="formtoprocess" name="formtoprocess" method="post" action="<?php print $host;?>">
-            <?php
-            	include('scripts/list_student_report.php');
-            ?>
-    		<div id="xml-checked-action" style="display:none;">
-                <params>
-                    <sids><?php print $sid;?></sids>
-                    <selectname>wrapper_rid</selectname>
-                </params>
-    		</div>
+	<form id="formtoprocess" name="formtoprocess" method="post" action="<?php print $host;?>">
+	    <?php
+		include('scripts/list_student_report.php');
+	    ?>
+		<div id="xml-checked-action" style="display:none;">
+		<params>
+		    <sids><?php print $sid;?></sids>
+		    <selectname>wrapper_rid</selectname>
+		</params>
+		</div>
 		<input type="hidden" name="cancel" value="<?php print $cancel;?>"/>
 		<input type="hidden" name="choice" value="<?php print $choice;?>"/>
 	  </form>
