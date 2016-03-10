582cf25f5653e1f9dbe0a241cea929f6a15a13bd
Marius Avram <marius@learningdata.ie>
2016-03-10 15:31:04 +0000

    removed commented school_conf_update execution

--- lib/functions.php
+++ lib/functions.php
@@ -2091,11 +2091,6 @@ function check_class_release(){
 			}
 		}
 
-	/*$class_update_path=$CFG->installpath."/".$CFG->applicationdirectory."/scripts/school_conf_update.php";
-	$argvs="--path=".$CFG->installpath;
-	$update_school_php="php $class_update_path $argvs 2>&1";
-	$result=exec(escapeshellcmd($update_school_php));*/
-
 	return $upgrade;
 	}
 
